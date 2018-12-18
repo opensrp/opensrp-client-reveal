93136773107c9f9961e1262a1dc2fe166d08a4b8
Manu Tarus <manutarus@gmail.com>
2018-12-18 13:09:05 +0300

    scheduling sync task

--- opensrp-reveal/src/main/java/org/smartregister/reveal/interactor/LoginInteractor.java
+++ opensrp-reveal/src/main/java/org/smartregister/reveal/interactor/LoginInteractor.java
@@ -1,8 +1,12 @@
 package org.smartregister.reveal.interactor;
 
 import org.smartregister.login.interactor.BaseLoginInteractor;
+import org.smartregister.reveal.BuildConfig;
+import org.smartregister.reveal.job.RevealCampaignServiceJob;
 import org.smartregister.reveal.util.Utils;
 import org.smartregister.view.contract.BaseLoginContract;
+
+import java.util.concurrent.TimeUnit;
 
 public class LoginInteractor extends BaseLoginInteractor implements BaseLoginContract.Interactor {
 
@@ -13,6 +17,7 @@ public class LoginInteractor extends BaseLoginInteractor implements BaseLoginCon
     @Override
     protected void scheduleJobs() {
         Utils.startImmediateSync();
+        RevealCampaignServiceJob.scheduleJob(RevealCampaignServiceJob.TAG, TimeUnit.MINUTES.toMillis(BuildConfig.SYNC_INTERVAL_IN_MINUTES), getFlexValue(BuildConfig.SYNC_INTERVAL_IN_MINUTES));
     }
 
 }
--- opensrp-reveal/src/main/java/org/smartregister/reveal/sync/RevealCampaignIntentService.java
+++ opensrp-reveal/src/main/java/org/smartregister/reveal/sync/RevealCampaignIntentService.java
@@ -6,12 +6,17 @@ import org.smartregister.domain.FetchStatus;
 import org.smartregister.reveal.job.LocationTaskServiceJob;
 import org.smartregister.reveal.util.AppExecutors;
 import org.smartregister.sync.intent.CampaignIntentService;
+import org.smartregister.util.NetworkUtils;
 import org.smartregister.util.Utils;
 
 public class RevealCampaignIntentService extends CampaignIntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
+        if (!NetworkUtils.isNetworkAvailable()) {
+            Utils.completeSync(FetchStatus.noConnection);
+            return;
+        }
         sendBroadcast(Utils.completeSync(FetchStatus.fetchStarted));
         super.onHandleIntent(intent);
         (new AppExecutors()).mainThread().execute(new Runnable() {
