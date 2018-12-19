c5f8afd71f0db7195ae938f96c35fa29eb2faaa9
Samuel Githengi <samuel.githengi@gmail.com>
2018-12-19 19:18:55 +0300

    add functionality for specifying custom textsize for native form edittext

--- opensrp-reveal/src/main/java/org/smartregister/reveal/widget/RevealEditTextFactory.java
+++ opensrp-reveal/src/main/java/org/smartregister/reveal/widget/RevealEditTextFactory.java
@@ -1,10 +1,15 @@
 package org.smartregister.reveal.widget;
 
 import android.content.Context;
+import android.content.res.Configuration;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.ImageView;
 
+import com.rengwuxian.materialedittext.MaterialEditText;
 import com.vijay.jsonwizard.fragments.JsonFormFragment;
 import com.vijay.jsonwizard.interfaces.CommonListener;
+import com.vijay.jsonwizard.utils.FormUtils;
 import com.vijay.jsonwizard.widgets.EditTextFactory;
 
 import org.json.JSONObject;
@@ -25,6 +30,23 @@ public class RevealEditTextFactory extends EditTextFactory {
     public List<View> getViewsFromJson(String stepName, Context context, JsonFormFragment formFragment, JSONObject jsonObject, CommonListener listener, boolean popup) throws Exception {
         hasNoPadding = new JSONObject(formFragment.getCurrentJsonState()).getJSONObject(stepName).optBoolean(NO_PADDING);
         return super.getViewsFromJson(stepName, context, formFragment, jsonObject, listener, popup);
+    }
+
+    @Override
+    protected void attachLayout(String stepName, Context context, JsonFormFragment formFragment, JSONObject jsonObject, MaterialEditText editText, ImageView editButton) throws Exception {
+        super.attachLayout(stepName, context, formFragment, jsonObject, editText, editButton);
+        Configuration config = context.getResources().getConfiguration();
+        int textSize = FormUtils.getValueFromSpOrDpOrPx(jsonObject.optString("text_size", String.valueOf(context.getResources().getDimension(com.vijay.jsonwizard.R
+                .dimen.default_text_size))), context);
+        if (config.smallestScreenWidthDp < 600) {
+            editText.setFloatingLabelTextSize((int) (textSize * 1.6));
+            editText.setTextSize(textSize);
+        } else {
+            editText.setFloatingLabelTextSize(textSize * 3);
+            editText.setTextSize(textSize * 2);
+        }
+        editText.setGravity(Gravity.START);
+        editText.setSingleLine(false);
     }
 
     @Override
