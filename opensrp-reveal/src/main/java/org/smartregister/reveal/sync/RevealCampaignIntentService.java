c8a4d92f24fa0e8cd3d125748fadab5c9289a225
Samuel Githengi <samuel.githengi@gmail.com>
2019-01-28 11:07:25 +0300

    Validate authorization before campaign sync

--- opensrp-reveal/src/main/java/org/smartregister/reveal/sync/RevealCampaignIntentService.java
+++ opensrp-reveal/src/main/java/org/smartregister/reveal/sync/RevealCampaignIntentService.java
@@ -7,15 +7,23 @@ import org.smartregister.reveal.job.LocationTaskServiceJob;
 import org.smartregister.reveal.util.AppExecutors;
 import org.smartregister.sync.intent.CampaignIntentService;
 import org.smartregister.util.NetworkUtils;
+import org.smartregister.util.SyncUtils;
 import org.smartregister.util.Utils;
 
 public class RevealCampaignIntentService extends CampaignIntentService {
+
+    private SyncUtils syncUtils;
 
     @Override
     protected void onHandleIntent(Intent intent) {
         if (!NetworkUtils.isNetworkAvailable()) {
             sendBroadcast(Utils.completeSync(FetchStatus.noConnection));
             return;
+        }
+        if (!syncUtils.verifyAuthorization()) {
+            syncUtils.logoutUser();
+            return;
+
         }
         sendBroadcast(Utils.completeSync(FetchStatus.fetchStarted));
         super.onHandleIntent(intent);
@@ -27,4 +35,9 @@ public class RevealCampaignIntentService extends CampaignIntentService {
         });
     }
 
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        syncUtils = new SyncUtils(getBaseContext());
+        return super.onStartCommand(intent, flags, startId);
+    }
 }
