ec4de85076be42e1682ca804f6e9fcc364604115
Samuel Githengi <samuel.githengi@gmail.com>
2019-03-07 10:48:20 +0300

    Upgrade Validator use string true,Upgrade to native form that validates checkboxes and radio buttons

--- opensrp-reveal/src/main/java/org/smartregister/reveal/presenter/RevealJsonFormFragmentPresenter.java
+++ opensrp-reveal/src/main/java/org/smartregister/reveal/presenter/RevealJsonFormFragmentPresenter.java
@@ -7,6 +7,7 @@ import com.vijay.jsonwizard.fragments.JsonFormFragment;
 import com.vijay.jsonwizard.interactors.JsonFormInteractor;
 import com.vijay.jsonwizard.presenters.JsonFormFragmentPresenter;
 import com.vijay.jsonwizard.utils.ValidationStatus;
+import com.vijay.jsonwizard.views.JsonFormFragmentView;
 
 import org.smartregister.reveal.view.RevealMapView;
 import org.smartregister.reveal.widget.GeoWidgetFactory;
@@ -24,18 +25,23 @@ public class RevealJsonFormFragmentPresenter extends JsonFormFragmentPresenter {
     }
 
     @Override
-    public ValidationStatus writeValuesAndValidate(LinearLayout mainView) {
-        ValidationStatus validationStatus = super.writeValuesAndValidate(mainView);
-        if (validationStatus.isValid()) {
+    public void validateAndWriteValues() {
+        super.validateAndWriteValues();
+        if (isFormValid()) {
             for (View childAt : formFragment.getJsonApi().getFormDataViews()) {
                 if (childAt instanceof RevealMapView) {
                     RevealMapView mapView = (RevealMapView) childAt;
-                    validationStatus = GeoWidgetFactory.validate(formFragment, mapView);
+                    ValidationStatus validationStatus = GeoWidgetFactory.validate(formFragment, mapView);
+                    if (!validationStatus.isValid()) {
+                        String key = (String) childAt.getTag(com.vijay.jsonwizard.R.id.key);
+                        String mStepName = this.getView().getArguments().getString("stepName");
+                        String fieldKey = mStepName + " (" + mStepDetails.optString("title") + ") :" + key;
+                        getInvalidFields().put(fieldKey, validationStatus);
+                    }
                 }
             }
 
         }
-        return validationStatus;
     }
 
 }
