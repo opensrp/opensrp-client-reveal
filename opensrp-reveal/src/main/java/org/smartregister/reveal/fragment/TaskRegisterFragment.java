0681e76f21cfbeec17b95b55b7bf8576bf4ef33d
Samuel Githengi <samuel.githengi@gmail.com>
2019-03-14 19:32:48 +0300

    Implement Task register toolbar

--- opensrp-reveal/src/main/java/org/smartregister/reveal/fragment/TaskRegisterFragment.java
+++ opensrp-reveal/src/main/java/org/smartregister/reveal/fragment/TaskRegisterFragment.java
@@ -1,9 +1,16 @@
 package org.smartregister.reveal.fragment;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 import org.smartregister.cursoradapter.RecyclerViewPaginatedAdapter;
 import org.smartregister.family.fragment.NoMatchDialogFragment;
+import org.smartregister.reveal.R;
 import org.smartregister.reveal.presenter.TaskRegisterFragmentPresenter;
 import org.smartregister.reveal.provider.TaskRegisterProvider;
 import org.smartregister.reveal.util.Constants;
@@ -19,6 +26,28 @@ import java.util.Set;
  * Created by samuelgithengi on 3/11/19.
  */
 public class TaskRegisterFragment extends BaseRegisterFragment implements BaseRegisterFragmentContract.View {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_task_register, container, false);
+        rootView = view;//handle to the root
+
+        Toolbar toolbar = view.findViewById(R.id.register_toolbar);
+        AppCompatActivity activity = ((AppCompatActivity) getActivity());
+
+        activity.setSupportActionBar(toolbar);
+        activity.getSupportActionBar().setTitle(activity.getIntent().getStringExtra(TOOLBAR_TITLE));
+        activity.getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+
+        activity.getSupportActionBar().setLogo(R.drawable.round_white_background);
+        activity.getSupportActionBar().setDisplayUseLogoEnabled(false);
+        activity.getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        setupViews(view);
+        return view;
+    }
 
     public void initializeAdapter(Set<org.smartregister.configurableviews.model.View> visibleColumns) {
         TaskRegisterProvider taskRegisterProvider = new TaskRegisterProvider(getActivity(), commonRepository(), visibleColumns, registerActionHandler, paginationViewHandler);
