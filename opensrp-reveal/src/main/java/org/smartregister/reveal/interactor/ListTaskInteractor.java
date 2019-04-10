402df646902048779c6ceaaeb84a57e0913dd6d8
Vincent Karuri <vincentmkaruri@gmail.com>
2019-04-10 16:42:30 +0300

    Refactor code to allow saving of larval dipping form

--- opensrp-reveal/src/main/java/org/smartregister/reveal/interactor/ListTaskInteractor.java
+++ opensrp-reveal/src/main/java/org/smartregister/reveal/interactor/ListTaskInteractor.java
@@ -59,7 +59,10 @@ import static com.cocoahero.android.geojson.Geometry.JSON_COORDINATES;
 import static org.smartregister.reveal.util.Constants.BusinessStatus.NOT_VISITED;
 import static org.smartregister.reveal.util.Constants.DETAILS;
 import static org.smartregister.reveal.util.Constants.Intervention.IRS;
+import static org.smartregister.reveal.util.Constants.Intervention.LARVAL_DIPPING;
 import static org.smartregister.reveal.util.Constants.Intervention.MOSQUITO_COLLECTION;
+import static org.smartregister.reveal.util.Constants.JsonForm.ENCOUNTER_TYPE;
+import static org.smartregister.reveal.util.Constants.LARVAL_DIPPING_EVENT;
 import static org.smartregister.reveal.util.Constants.METADATA;
 import static org.smartregister.reveal.util.Constants.MOSQUITO_COLLECTION_EVENT;
 import static org.smartregister.reveal.util.Constants.REGISTER_STRUCTURE_EVENT;
@@ -283,13 +286,11 @@ public class ListTaskInteractor {
     public void saveJsonForm(String json) {
         try {
             JSONObject jsonForm = new JSONObject(json);
-            String encounterType = jsonForm.getString(JsonForm.ENCOUNTER_TYPE);
-            if (SPRAY_EVENT.equals(encounterType)) {
-                saveSprayForm(jsonForm);
+            String encounterType = jsonForm.getString(ENCOUNTER_TYPE);
+            if (SPRAY_EVENT.equals(encounterType) || MOSQUITO_COLLECTION_EVENT.equals(encounterType) || LARVAL_DIPPING_EVENT.equals(encounterType)) {
+                saveMosquitoInterventionForm(jsonForm);
             } else if (REGISTER_STRUCTURE_EVENT.equals(encounterType)) {
                 saveRegisterStructureForm(jsonForm);
-            } else if (MOSQUITO_COLLECTION_EVENT.equals(encounterType)) {
-                saveMosquitoCollectionForm(jsonForm);
             }
         } catch (Exception e) {
             Log.e(TAG, "Error saving Json Form data", e);
@@ -311,30 +312,6 @@ public class ListTaskInteractor {
         eventJson.put(DETAILS, getJSONObject(jsonForm, DETAILS));
         eventClientRepository.addEvent(entityId, eventJson);
         return gson.fromJson(eventJson.toString(), org.smartregister.domain.db.Event.class);
-    }
-
-    private void saveSprayForm(JSONObject jsonForm) {
-        Runnable runnable = new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    org.smartregister.domain.db.Event event = saveEvent(jsonForm, SPRAY_EVENT, STRUCTURE);
-                    clientProcessor.processClient(Collections.singletonList(new EventClient(event, null)), true);
-                    appExecutors.mainThread().execute(new Runnable() {
-                        @Override
-                        public void run() {
-                            String businessStatus = clientProcessor.calculateBusinessStatus(event);
-                            presenterCallBack.onFormSaved(event.getBaseEntityId(), Task.TaskStatus.COMPLETED, businessStatus, IRS);
-                        }
-                    });
-                } catch (JSONException e) {
-                    Log.e(TAG, "Error saving spraye", e);
-                    presenterCallBack.onFormSaveFailure(SPRAY_EVENT);
-                }
-            }
-        };
-
-        appExecutors.diskIO().execute(runnable);
     }
 
     private void saveRegisterStructureForm(JSONObject jsonForm) {
@@ -421,25 +398,45 @@ public class ListTaskInteractor {
         appExecutors.diskIO().execute(runnable);
     }
 
-    private void saveMosquitoCollectionForm(JSONObject jsonForm) {
+
+    private void saveMosquitoInterventionForm(JSONObject jsonForm) {
+        String interventionType = null;
+        String encounterType = null;
+        try {
+            encounterType = jsonForm.getString(ENCOUNTER_TYPE);
+            if (encounterType.equals(SPRAY_EVENT)) {
+                interventionType = IRS;
+            } else if (encounterType.equals(MOSQUITO_COLLECTION_EVENT)) {
+                interventionType = MOSQUITO_COLLECTION;
+            } else if (encounterType.equals(LARVAL_DIPPING_EVENT)) {
+                interventionType = LARVAL_DIPPING;
+            }
+        } catch (JSONException e) {
+            Log.e(TAG, e.getStackTrace().toString());
+        }
+
+        final String finalInterventionType = interventionType;
+        final String finalEncounterType = encounterType;
         Runnable runnable = new Runnable() {
             @Override
             public void run() {
                 try {
-                    org.smartregister.domain.db.Event event = saveEvent(jsonForm, MOSQUITO_COLLECTION_EVENT, STRUCTURE);
+                    org.smartregister.domain.db.Event event = saveEvent(jsonForm, finalEncounterType, STRUCTURE);
                     clientProcessor.processClient(Collections.singletonList(new EventClient(event, null)), true);
                     appExecutors.mainThread().execute(new Runnable() {
                         @Override
                         public void run() {
                             String businessStatus = clientProcessor.calculateBusinessStatus(event);
-                            presenterCallBack.onFormSaved(event.getBaseEntityId(), Task.TaskStatus.COMPLETED, businessStatus, MOSQUITO_COLLECTION);
+                            presenterCallBack.onFormSaved(event.getBaseEntityId(), Task.TaskStatus.COMPLETED, businessStatus, finalInterventionType);
                         }
                     });
-                } catch (Exception e) {
-                    Log.e(TAG, "Error saving mosquito collection point data");
+                } catch (JSONException e) {
+                    Log.e(TAG, "Error saving spraye", e);
+                    presenterCallBack.onFormSaveFailure(finalEncounterType);
                 }
             }
         };
+
         appExecutors.diskIO().execute(runnable);
     }
 }
