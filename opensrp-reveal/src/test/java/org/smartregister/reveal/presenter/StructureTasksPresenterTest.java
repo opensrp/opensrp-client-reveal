053204d5e2c436552530d624e74c16f08550fe6e
Samuel Githengi <samuel.githengi@gmail.com>
2019-05-22 16:07:40 +0300

    Code cleanup

--- opensrp-reveal/src/test/java/org/smartregister/reveal/presenter/StructureTasksPresenterTest.java
+++ opensrp-reveal/src/test/java/org/smartregister/reveal/presenter/StructureTasksPresenterTest.java
@@ -24,10 +24,8 @@ import java.util.List;
 import java.util.UUID;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.timeout;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
