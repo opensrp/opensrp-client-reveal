b4667bbbec5f3a885728bc7ed484e81c4b437e58
Samuel Githengi <samuel.githengi@gmail.com>
2019-05-24 18:25:41 +0300

    Implement edit family members

--- opensrp-reveal/src/main/java/org/smartregister/reveal/view/FamilyOtherMemberProfileActivity.java
+++ opensrp-reveal/src/main/java/org/smartregister/reveal/view/FamilyOtherMemberProfileActivity.java
@@ -1,16 +1,30 @@
 package org.smartregister.reveal.view;
 
+import android.content.Intent;
 import android.support.v4.view.ViewPager;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.vijay.jsonwizard.constants.JsonFormConstants;
+import com.vijay.jsonwizard.domain.Form;
+
+import org.json.JSONObject;
+import org.smartregister.commonregistry.CommonPersonObject;
+import org.smartregister.commonregistry.CommonPersonObjectClient;
+import org.smartregister.commonregistry.CommonRepository;
 import org.smartregister.family.activity.BaseFamilyOtherMemberProfileActivity;
 import org.smartregister.family.adapter.ViewPagerAdapter;
 import org.smartregister.family.model.BaseFamilyOtherMemberProfileActivityModel;
 import org.smartregister.family.presenter.BaseFamilyOtherMemberProfileActivityPresenter;
 import org.smartregister.family.util.Constants;
+import org.smartregister.family.util.JsonFormUtils;
+import org.smartregister.family.util.Utils;
 import org.smartregister.reveal.R;
+import org.smartregister.reveal.application.RevealApplication;
 import org.smartregister.reveal.fragment.FamilyOtherMemberProfileFragment;
+import org.smartregister.reveal.util.FamilyJsonFormUtils;
+
+import timber.log.Timber;
 
 public class FamilyOtherMemberProfileActivity extends BaseFamilyOtherMemberProfileActivity {
     @Override
@@ -43,6 +57,7 @@ public class FamilyOtherMemberProfileActivity extends BaseFamilyOtherMemberProfi
             addMember.setVisible(false);
         }
 
+        getMenuInflater().inflate(R.menu.other_member_menu, menu);
         return true;
     }
 
@@ -52,9 +67,66 @@ public class FamilyOtherMemberProfileActivity extends BaseFamilyOtherMemberProfi
             case android.R.id.home:
                 onBackPressed();
                 return true;
+            case R.id.action_registration:
+                startFormForEdit(R.string.edit_member_form_title);
+                return true;
             default:
                 break;
         }
         return super.onOptionsItemSelected(item);
     }
+
+    public void startFormForEdit(Integer title_resource) {
+
+        CommonRepository commonRepository = RevealApplication.getInstance().getContext().commonrepository(Utils.metadata().familyMemberRegister.tableName);
+
+        presenter().fetchProfileData();
+        final CommonPersonObject personObject = commonRepository.findByBaseEntityId(commonPersonObject.getCaseId());
+        final CommonPersonObjectClient client =
+                new CommonPersonObjectClient(personObject.getCaseId(), personObject.getDetails(), "");
+        client.setColumnmaps(personObject.getColumnmaps());
+
+        JSONObject form = FamilyJsonFormUtils.getAutoPopulatedJsonEditMemberFormString(
+                (title_resource != null) ? getResources().getString(title_resource) : null,
+                org.smartregister.chw.util.Constants.JSON_FORM.FAMILY_MEMBER_REGISTER,
+                this, client, org.smartregister.chw.util.Utils.metadata().familyMemberRegister.updateEventType, familyName, commonPersonObject.getCaseId().equalsIgnoreCase(primaryCaregiver));
+        try {
+            startFormActivity(form);
+        } catch (Exception e) {
+            Timber.e(e.getMessage());
+        }
+    }
+
+    public void startFormActivity(JSONObject jsonForm) {
+
+        Intent intent = new Intent(this, Utils.metadata().familyMemberFormActivity);
+        intent.putExtra(Constants.JSON_FORM_EXTRA.JSON, jsonForm.toString());
+
+
+        Form form = new Form();
+        form.setActionBarBackground(R.color.family_actionbar);
+        form.setWizard(false);
+        intent.putExtra(JsonFormConstants.JSON_FORM_KEY.FORM, form);
+
+
+        startActivityForResult(intent, JsonFormUtils.REQUEST_CODE_GET_JSON);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == JsonFormUtils.REQUEST_CODE_GET_JSON && resultCode == RESULT_OK) {
+            try {
+                String jsonString = data.getStringExtra(Constants.JSON_FORM_EXTRA.JSON);
+                JSONObject form = new JSONObject(jsonString);
+                if (form.getString(JsonFormUtils.ENCOUNTER_TYPE).equals(Utils.metadata().familyMemberRegister.updateEventType)) {
+                    presenter().updateFamilyMember(jsonString);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
 }
