c82cea9a4805fc9c6d6f8bac5e9339b7a4b7291a
Samuel Githengi <samuel.githengi@gmail.com>
2019-06-25 15:10:09 +0300

    Add addl fields for case details, add toolbar

--- opensrp-reveal/src/main/java/org/smartregister/reveal/fragment/CaseClassificationFragment.java
+++ opensrp-reveal/src/main/java/org/smartregister/reveal/fragment/CaseClassificationFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +17,15 @@ public class CaseClassificationFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-       return  inflater.inflate(R.layout.frament_case_classification, container, false);
+        View rootView = inflater.inflate(R.layout.frament_case_classification, container, false);
+
+        Toolbar toolbar = rootView.findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.return_to_register);
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        activity.setSupportActionBar(toolbar);
+        activity.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        return rootView;
     }
+
 }
