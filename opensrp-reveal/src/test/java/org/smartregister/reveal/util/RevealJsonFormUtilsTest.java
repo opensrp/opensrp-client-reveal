c2fcbdee17fac6625312cb70c0243c7d41866c55
Samuel Githengi <samuel.githengi@gmail.com>
2019-07-29 13:14:12 +0300

    Implement form utils unit tests

--- opensrp-reveal/src/test/java/org/smartregister/reveal/util/RevealJsonFormUtilsTest.java
+++ opensrp-reveal/src/test/java/org/smartregister/reveal/util/RevealJsonFormUtilsTest.java
@@ -2,9 +2,16 @@ package org.smartregister.reveal.util;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.reflect.Whitebox;
+import org.smartregister.reveal.BuildConfig;
 import org.smartregister.reveal.util.Constants.JsonForm;
 
 import static org.junit.Assert.assertEquals;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.when;
 import static org.smartregister.reveal.util.Constants.Intervention.LARVAL_DIPPING;
 import static org.smartregister.reveal.util.Constants.Intervention.MOSQUITO_COLLECTION;
 import static org.smartregister.reveal.util.Constants.LARVAL_DIPPING_EVENT;
@@ -40,5 +47,17 @@ public class RevealJsonFormUtilsTest {
     @Test
     public void testGetFormNameShouldReturnThailandMosquitoCollectionFormForLarvalDippingIntervention() {
         assertEquals(revealJsonFormUtils.getFormName(null, MOSQUITO_COLLECTION), JsonForm.MOSQUITO_COLLECTION_FORM);
+    }
+
+    @Test
+    public void testGetFormNameShouldReturnPAOTForm() {
+        Whitebox.setInternalState(BuildConfig.class, BuildConfig.BUILD_COUNTRY, Country.ZAMBIA);
+        assertEquals(JsonForm.PAOT_FORM, revealJsonFormUtils.getFormName(null, Constants.EventType.PAOT_EVENT));
+    }
+
+    @Test
+    public void testGetFormNameShouldReturnThailandPAOTForm() {
+        Whitebox.setInternalState(BuildConfig.class, BuildConfig.BUILD_COUNTRY, Country.THAILAND);
+        assertEquals(JsonForm.THAILAND_PAOT_FORM, revealJsonFormUtils.getFormName(null, Constants.EventType.PAOT_EVENT));
     }
 }
