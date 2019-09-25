eef7ad9f3286a710ffd68f1ea85f5864cbb49495
Martin Ndegwa <ndegwamartin@gmail.com>
2019-09-25 13:39:59 +0300

    Refactor view all indicators implementation Refactor indicators processing Fix crash on non-IRS plans viewage

---
+++ opensrp-reveal/src/main/java/org/smartregister/reveal/model/IndicatorDetails.java
@@ -0,0 +1,64 @@
+package org.smartregister.reveal.model;
+
+import java.util.List;
+
+/**
+ * Created by ndegwamartin on 2019-09-25.
+ */
+public class IndicatorDetails {
+
+    int sprayed;
+    int notSprayed;
+    int totalStructures;
+    int progress;
+    int notVisited;
+    List<String> sprayIndicatorList;
+
+    public int getSprayed() {
+        return sprayed;
+    }
+
+    public void setSprayed(int sprayed) {
+        this.sprayed = sprayed;
+    }
+
+    public int getNotSprayed() {
+        return notSprayed;
+    }
+
+    public void setNotSprayed(int notSprayed) {
+        this.notSprayed = notSprayed;
+    }
+
+    public int getTotalStructures() {
+        return totalStructures;
+    }
+
+    public void setTotalStructures(int totalStructures) {
+        this.totalStructures = totalStructures;
+    }
+
+    public int getProgress() {
+        return progress;
+    }
+
+    public void setProgress(int progress) {
+        this.progress = progress;
+    }
+
+    public int getNotVisited() {
+        return notVisited;
+    }
+
+    public void setNotVisited(int notVisited) {
+        this.notVisited = notVisited;
+    }
+
+    public List<String> getSprayIndicatorList() {
+        return sprayIndicatorList;
+    }
+
+    public void setSprayIndicatorList(List<String> sprayIndicatorList) {
+        this.sprayIndicatorList = sprayIndicatorList;
+    }
+}
--- opensrp-reveal/src/main/java/org/smartregister/reveal/task/IndicatorsCalculatorTask.java
+++ opensrp-reveal/src/main/java/org/smartregister/reveal/task/IndicatorsCalculatorTask.java
@@ -4,24 +4,27 @@ import android.app.Activity;
 import android.os.AsyncTask;
 
 import org.smartregister.reporting.view.ProgressIndicatorView;
+import org.smartregister.reporting.view.TableView;
 import org.smartregister.reveal.R;
+import org.smartregister.reveal.model.IndicatorDetails;
 import org.smartregister.reveal.model.TaskDetails;
 import org.smartregister.reveal.util.Constants;
 
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Created by ndegwamartin on 2019-09-23.
  */
-public class IndicatorsCalculatorTask extends AsyncTask<Void, Void, Map<String, Integer>> {
+public class IndicatorsCalculatorTask extends AsyncTask<Void, Void, IndicatorDetails> {
 
     private ProgressIndicatorView progressIndicator;
     private ProgressIndicatorView progressIndicator2;
     private ProgressIndicatorView progressIndicator3;
     private Activity activity;
     private List<TaskDetails> tasks;
+    TableView tableView;
 
     public IndicatorsCalculatorTask(Activity context, List<TaskDetails> tasks) {
         this.activity = context;
@@ -34,49 +37,93 @@ public class IndicatorsCalculatorTask extends AsyncTask<Void, Void, Map<String, 
         progressIndicator = activity.findViewById(R.id.progressIndicatorView);
         progressIndicator2 = activity.findViewById(R.id.progressIndicatorView2);
         progressIndicator3 = activity.findViewById(R.id.progressIndicatorView3);
+        tableView = activity.findViewById(R.id.tableView);
+
     }
 
     @Override
-    protected Map<String, Integer> doInBackground(Void... params) {
+    protected IndicatorDetails doInBackground(Void... params) {
 
-        Map<String, Integer> map = new HashMap<>();
+        IndicatorDetails indicatorDetails = new IndicatorDetails();
 
         for (int i = 0; i < this.tasks.size(); i++) {
 
-            String key = this.tasks.get(i).getSprayStatus() == null ? Constants.NULL_KEY : this.tasks.get(i).getSprayStatus();
 
-            int newValue = map.get(key) != null ? map.get(key) + 1 : 1;
+            if (Constants.BusinessStatus.SPRAYED.equals(this.tasks.get(i).getSprayStatus())) {
 
-            map.put(key, newValue);
+                indicatorDetails.setSprayed(indicatorDetails.getSprayed() + 1);
+
+            } else if (Constants.BusinessStatus.NOT_SPRAYED.equals(this.tasks.get(i).getSprayStatus())) {
+
+                indicatorDetails.setNotSprayed(indicatorDetails.getNotSprayed() + 1);
+
+            } else {
+
+                indicatorDetails.setNotVisited(indicatorDetails.getNotVisited() + 1);
+            }
 
         }
-        return map;
+
+        int totalStructures = this.tasks.size();
+        int progress = totalStructures > 0 ? Math.round(indicatorDetails.getSprayed() * 100 / totalStructures) : 0;
+
+
+        indicatorDetails.setTotalStructures(totalStructures);
+        indicatorDetails.setProgress(progress);
+
+        List<String> sprayIndicator = new ArrayList<>();
+
+        sprayIndicator.add(this.activity.getResources().getString(R.string.spray_coverage));
+        sprayIndicator.add(this.activity.getResources().getString(R.string.n_percent, progress));
+
+        int totalFound = (indicatorDetails.getSprayed() + indicatorDetails.getNotSprayed());
+
+        sprayIndicator.add(this.activity.getResources().getString(R.string.structures_remaining_90));
+        sprayIndicator.add(String.valueOf(Math.round(totalStructures * 0.9) - indicatorDetails.getSprayed()));
+
+
+        sprayIndicator.add(this.activity.getResources().getString(R.string.total_structures));
+        sprayIndicator.add(String.valueOf(totalStructures));
+
+
+        sprayIndicator.add(this.activity.getResources().getString(R.string.structures_not_visited));
+        sprayIndicator.add(String.valueOf(indicatorDetails.getNotVisited()));
+
+
+        sprayIndicator.add(this.activity.getResources().getString(R.string.structures_visited_found));
+        sprayIndicator.add(String.valueOf(totalFound));
+
+
+        sprayIndicator.add(this.activity.getResources().getString(R.string.sprayed));
+        sprayIndicator.add(String.valueOf(indicatorDetails.getSprayed()));
+
+        sprayIndicator.add(this.activity.getResources().getString(R.string.structures_not_sprayed));
+        sprayIndicator.add(String.valueOf(indicatorDetails.getNotSprayed()));
+
+        indicatorDetails.setSprayIndicatorList(sprayIndicator);
+
+        return indicatorDetails;
     }
 
     @Override
-    protected void onPostExecute(Map<String, Integer> map) {
+    protected void onPostExecute(IndicatorDetails indicatorDetails) {
 
-        Integer sprayedValue = map.get(Constants.BusinessStatus.SPRAYED);
-        Integer notSprayedValue = map.get(Constants.BusinessStatus.NOT_SPRAYED);
 
-        int sprayed = sprayedValue != null ? sprayedValue : 0;
-        int notSprayed = notSprayedValue != null ? notSprayedValue : 0;
-        int totalStructures = this.tasks.size();
-        int progress = totalStructures > 0 ? Math.round(sprayed * 100 / totalStructures) : 0;
+        progressIndicator.setProgress(indicatorDetails.getProgress());
+        progressIndicator.setTitle(this.activity.getString(R.string.n_percent, indicatorDetails.getProgress()));
 
-        progressIndicator.setProgress(progress);
-        progressIndicator.setTitle(progress + "%");
-
-        int totalFound = (sprayed + notSprayed);
-        int progress2 = totalStructures > 0 ? Math.round(totalFound * 100 / totalStructures) : 0;
+        int totalFound = indicatorDetails.getSprayed() + indicatorDetails.getNotSprayed();
+        int progress2 = indicatorDetails.getTotalStructures() > 0 ? Math.round(totalFound * 100 / indicatorDetails.getTotalStructures()) : 0;
 
         progressIndicator2.setProgress(progress2);
-        progressIndicator2.setTitle(progress2 + "%");
+        progressIndicator2.setTitle(this.activity.getString(R.string.n_percent, progress2));
 
-        int progress3 = totalFound > 0 ? Math.round((sprayed * 100 / totalFound)) : 0;
+        int progress3 = totalFound > 0 ? Math.round((indicatorDetails.getSprayed() * 100 / totalFound)) : 0;
 
         progressIndicator3.setProgress(progress3);
-        progressIndicator3.setTitle(progress3 + "%");
+        progressIndicator3.setTitle(this.activity.getString(R.string.n_percent, progress3));
+
+        tableView.setTableData(Arrays.asList(new String[]{this.activity.getString(R.string.indicator), this.activity.getString(R.string.value)}), indicatorDetails.getSprayIndicatorList());
 
     }
 }
