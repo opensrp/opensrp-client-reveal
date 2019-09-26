01866adb66d3820e7f873e5fcef9f19ea22458a2
Samuel Githengi <samuel.githengi@gmail.com>
2019-09-26 11:54:13 +0300

    Show edit icon for complete bed distribution and screening forms

--- opensrp-reveal/src/main/java/org/smartregister/reveal/viewholder/StructureTaskViewHolder.java
+++ opensrp-reveal/src/main/java/org/smartregister/reveal/viewholder/StructureTaskViewHolder.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.smartregister.reveal.R;
@@ -11,6 +12,8 @@ import org.smartregister.reveal.model.CardDetails;
 import org.smartregister.reveal.model.StructureTaskDetails;
 import org.smartregister.reveal.util.CardDetailsUtil;
 import org.smartregister.reveal.util.Constants;
+import org.smartregister.reveal.util.Constants.BusinessStatus;
+import org.smartregister.reveal.util.Constants.Intervention;
 
 /**
  * Created by samuelgithengi on 4/11/19.
@@ -23,11 +26,14 @@ public class StructureTaskViewHolder extends RecyclerView.ViewHolder {
 
     private TextView actionTextView;
 
+    private ImageView viewEditImageView;
+
     public StructureTaskViewHolder(@NonNull View itemView) {
         super(itemView);
         context = itemView.getContext();
         nameTextView = itemView.findViewById(R.id.task_name);
         actionTextView = itemView.findViewById(R.id.task_action);
+        viewEditImageView = itemView.findViewById(R.id.view_edit);
     }
 
     public void setTaskName(String name) {
@@ -35,7 +41,7 @@ public class StructureTaskViewHolder extends RecyclerView.ViewHolder {
     }
 
     public void setTaskAction(StructureTaskDetails taskDetails, View.OnClickListener onClickListener) {
-        if (!Constants.BusinessStatus.NOT_VISITED.equals(taskDetails.getBusinessStatus())) {
+        if (!BusinessStatus.NOT_VISITED.equals(taskDetails.getBusinessStatus())) {
             actionTextView.setText(CardDetailsUtil.getTranslatedBusinessStatus(taskDetails.getBusinessStatus()));
             actionTextView.setBackground(null);
             CardDetails cardDetails = new CardDetails(taskDetails.getBusinessStatus());
@@ -45,6 +51,14 @@ public class StructureTaskViewHolder extends RecyclerView.ViewHolder {
             actionTextView.setText(taskDetails.getTaskAction());
             actionTextView.setBackground(context.getResources().getDrawable(R.drawable.structure_task_action_bg));
             actionTextView.setTextColor(context.getResources().getColor(R.color.task_not_done));
+        }
+
+        if (BusinessStatus.COMPLETE.equals(taskDetails.getBusinessStatus()) &&
+                (Intervention.BEDNET_DISTRIBUTION.equals(taskDetails.getTaskCode()) || Intervention.BLOOD_SCREENING.equals(taskDetails.getTaskCode()))) {
+            viewEditImageView.setVisibility(View.VISIBLE);
+            viewEditImageView.setOnClickListener(onClickListener);
+        } else {
+            viewEditImageView.setVisibility(View.GONE);
         }
         actionTextView.setOnClickListener(onClickListener);
         actionTextView.setTag(R.id.task_details, taskDetails);
