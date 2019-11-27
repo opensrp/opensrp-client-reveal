a9d795713eaec4d3ad50d9fff068d235de4acf32
Rodgers Andati <andatirodgers@gmail.com>
2019-11-27 12:57:30 +0300

    add functionality to copy database to Downloads folder

--- opensrp-reveal/src/main/java/org/smartregister/reveal/view/DrawerMenuView.java
+++ opensrp-reveal/src/main/java/org/smartregister/reveal/view/DrawerMenuView.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.view.ViewTreeObserver;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.vijay.jsonwizard.customviews.TreeViewDialog;
 
@@ -145,6 +146,14 @@ public class DrawerMenuView implements View.OnClickListener, BaseDrawerContract.
         headerView.findViewById(R.id.logout_button).setOnClickListener(this);
         headerView.findViewById(R.id.sync_button).setOnClickListener(this);
 
+        operatorTextView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(getContext(), "Copying Database", Toast.LENGTH_LONG).show();
+                org.smartregister.reveal.util.Utils.copyDatabase();
+                return false;
+            }
+        });
     }
 
     @Override
