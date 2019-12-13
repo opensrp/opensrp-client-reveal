398119f561d2d36959878f7284bc4ebc6e22954a
Samuel Githengi <samuel.githengi@gmail.com>
2019-12-13 18:38:17 +0300

    add archive family presenter unit tests

--- opensrp-reveal/src/test/java/org/smartregister/reveal/interactor/RevealFamilyProfileInteractorTest.java
+++ opensrp-reveal/src/test/java/org/smartregister/reveal/interactor/RevealFamilyProfileInteractorTest.java
@@ -40,7 +40,6 @@ import org.smartregister.sync.ClientProcessorForJava;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.Executors;
