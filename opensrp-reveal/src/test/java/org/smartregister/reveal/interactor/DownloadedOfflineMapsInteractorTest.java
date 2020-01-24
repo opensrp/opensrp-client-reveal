a2558361d2755a866d22b44f038a7bb834418674
richard <rkareko@ona.io>
2020-01-24 11:59:51 +0300

    Remove unused variables

--- opensrp-reveal/src/test/java/org/smartregister/reveal/interactor/DownloadedOfflineMapsInteractorTest.java
+++ opensrp-reveal/src/test/java/org/smartregister/reveal/interactor/DownloadedOfflineMapsInteractorTest.java
@@ -2,12 +2,9 @@ package org.smartregister.reveal.interactor;
 
 import android.content.Context;
 
-import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
@@ -17,13 +14,11 @@ import org.smartregister.domain.Location;
 import org.smartregister.repository.LocationRepository;
 import org.smartregister.reveal.BaseUnitTest;
 import org.smartregister.reveal.contract.DownloadedOfflineMapsContract;
-import org.smartregister.reveal.model.OfflineMapModel;
 
 import java.util.Collections;
 import java.util.List;
 
 import io.ona.kujaku.data.realm.RealmDatabase;
-import io.ona.kujaku.data.realm.objects.MapBoxOfflineQueueTask;
 
 import static org.mockito.ArgumentMatchers.anyObject;
 import static org.mockito.Mockito.verify;
@@ -47,15 +42,6 @@ public class DownloadedOfflineMapsInteractorTest extends BaseUnitTest {
 
     @Mock
     private RealmDatabase realmDatabase;
-
-    @Captor
-    private ArgumentCaptor<List<String>> locationIdsCaptor;
-
-    @Captor
-    private ArgumentCaptor<List<OfflineMapModel>> offlineMapModelListArgumentCaptor;
-
-    @Captor
-    private ArgumentCaptor<Boolean> booleanArgumentCaptor;
 
     private Context context = RuntimeEnvironment.application;
 
@@ -89,16 +75,6 @@ public class DownloadedOfflineMapsInteractorTest extends BaseUnitTest {
         when(realmDatabase.getTasks()).thenReturn(null);
         interactor.fetchLocationsWithOfflineMapDownloads(anyObject());
         verify(presenter).onOAsWithOfflineDownloadsFetched(null);
-    }
-
-    private List<MapBoxOfflineQueueTask> initOfflineQueueTasks() {
-
-        MapBoxOfflineQueueTask offlineQueueTask = new MapBoxOfflineQueueTask();
-        offlineQueueTask.setTask(new JSONObject());
-        offlineQueueTask.setTaskStatus(MapBoxOfflineQueueTask.TASK_STATUS_DONE);
-        offlineQueueTask.setTaskType(MapBoxOfflineQueueTask.TASK_TYPE_DOWNLOAD);
-
-        return Collections.singletonList(offlineQueueTask);
     }
 
     private Location initLocation() {
