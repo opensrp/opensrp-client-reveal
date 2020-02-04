a0523b7c9a26a68f61d15d037fe324d5fa0f3aa2
Samuel Githengi <samuel.githengi@gmail.com>
2020-02-04 16:01:25 +0300

    Test reveal map helper using roboelectric

---
+++ opensrp-reveal/src/test/java/org/smartregister/reveal/shadow/LayerShadow.java
@@ -0,0 +1,33 @@
+package org.smartregister.reveal.shadow;
+
+import android.support.annotation.NonNull;
+
+import com.mapbox.mapboxsdk.style.layers.Layer;
+import com.mapbox.mapboxsdk.style.layers.PropertyValue;
+import com.mapbox.mapboxsdk.style.layers.SymbolLayer;
+
+import org.robolectric.annotation.Implementation;
+import org.robolectric.annotation.Implements;
+
+/**
+ * Created by samuelgithengi on 2/4/20.
+ */
+@Implements(Layer.class)
+public class LayerShadow {
+
+    private String layerId;
+
+    @Implementation
+    public void setProperties(@NonNull PropertyValue<?>... properties) {
+        // Do nothing
+    }
+
+    @Implementation
+    public String getId() {
+        return layerId;
+    }
+
+    public void setLayerId(String layerId) {
+        this.layerId = layerId;
+    }
+}
