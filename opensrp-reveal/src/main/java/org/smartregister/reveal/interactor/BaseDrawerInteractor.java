ee26fa369ea454303af2b7757e9832f65c1bcc7c
Samuel Githengi <samuel.githengi@gmail.com>
2020-02-04 17:23:40 +0300

    reset plan if its not valid for selected jurisdiction

--- opensrp-reveal/src/main/java/org/smartregister/reveal/interactor/BaseDrawerInteractor.java
+++ opensrp-reveal/src/main/java/org/smartregister/reveal/interactor/BaseDrawerInteractor.java
@@ -3,10 +3,12 @@ package org.smartregister.reveal.interactor;
 import org.smartregister.domain.Location;
 import org.smartregister.domain.PlanDefinition;
 import org.smartregister.repository.LocationRepository;
+import org.smartregister.repository.PlanDefinitionRepository;
 import org.smartregister.repository.PlanDefinitionSearchRepository;
 import org.smartregister.reveal.application.RevealApplication;
 import org.smartregister.reveal.contract.BaseDrawerContract;
 import org.smartregister.reveal.util.AppExecutors;
+import org.smartregister.reveal.util.Utils;
 
 import java.util.Set;
 
@@ -20,6 +22,7 @@ public class BaseDrawerInteractor implements BaseDrawerContract.Interactor {
     private BaseDrawerContract.Presenter presenter;
 
     private PlanDefinitionSearchRepository planDefinitionSearchRepository;
+
 
     private LocationRepository locationRepository;
 
@@ -35,7 +38,7 @@ public class BaseDrawerInteractor implements BaseDrawerContract.Interactor {
         Runnable runnable = new Runnable() {
             @Override
             public void run() {
-                Location operationalArea  = locationRepository.getLocationByName(jurisdictionName);
+                Location operationalArea = locationRepository.getLocationByName(jurisdictionName);
                 String jurisdictionIdentifier = operationalArea != null ? operationalArea.getId() : null;
                 Set<PlanDefinition> planDefinitionSet = planDefinitionSearchRepository.findActivePlansByJurisdiction(jurisdictionIdentifier);
                 appExecutors.mainThread().execute(new Runnable() {
@@ -49,5 +52,22 @@ public class BaseDrawerInteractor implements BaseDrawerContract.Interactor {
         };
 
         appExecutors.diskIO().execute(runnable);
+    }
+
+    @Override
+    public void validateCurrentPlan(String selectedOperationalArea, String currentPlanId) {
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                Location operationalArea = Utils.getOperationalAreaLocation(selectedOperationalArea);
+                String jurisdictionIdentifier = operationalArea != null ? operationalArea.getId() : null;
+                boolean isValid = planDefinitionSearchRepository.planExists(currentPlanId, jurisdictionIdentifier);
+                appExecutors.mainThread().execute(() -> presenter.onPlanValidated(isValid));
+
+            }
+        };
+
+        appExecutors.diskIO().execute(runnable);
+
     }
 }
