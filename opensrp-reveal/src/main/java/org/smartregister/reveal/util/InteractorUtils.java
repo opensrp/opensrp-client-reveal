8709166e3908f422cb29ce6781a960911ccc6051
Samuel Githengi <samuel.githengi@gmail.com>
2020-02-07 14:27:03 +0300

    Use date Archived from archive events to archive the client locally

--- opensrp-reveal/src/main/java/org/smartregister/reveal/util/InteractorUtils.java
+++ opensrp-reveal/src/main/java/org/smartregister/reveal/util/InteractorUtils.java
@@ -95,7 +95,7 @@ public class InteractorUtils {
 
             Event archiveEvent = FamilyJsonFormUtils.createFamilyEvent(baseEntityId, Utils.getCurrentLocationId(),
                     null, isFamily ? EventType.ARCHIVE_FAMILY : EventType.ARCHIVE_FAMILY_MEMBER);
-            archiveEvent.addObs(new Obs().withValue(now).withFieldCode("dateRemoved").withFieldType("formsubmissionField"));
+            archiveEvent.addObs(new Obs().withValue(now).withFieldCode("dateArchived").withFieldType("formsubmissionField"));
 
             JSONObject eventJson = new JSONObject(gson.toJson(archiveEvent));
             eventJson.put(EventClientRepository.event_column.syncStatus.name(), BaseRepository.TYPE_Unsynced);
