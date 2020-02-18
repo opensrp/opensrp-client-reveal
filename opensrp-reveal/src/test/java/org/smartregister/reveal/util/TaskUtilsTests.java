381be8e0c5eab4e1fd9ef8612a7975889989e6f7
richard <rkareko@ona.io>
2020-02-18 12:51:12 +0300

    Add generate task test

--- opensrp-reveal/src/test/java/org/smartregister/reveal/util/TaskUtilsTests.java
+++ opensrp-reveal/src/test/java/org/smartregister/reveal/util/TaskUtilsTests.java
@@ -1,5 +1,7 @@
 package org.smartregister.reveal.util;
 
+import android.content.Context;
+
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -12,14 +14,17 @@ import org.powermock.reflect.Whitebox;
 import org.robolectric.RuntimeEnvironment;
 import org.smartregister.domain.Location;
 import org.smartregister.domain.Task;
+import org.smartregister.repository.BaseRepository;
 import org.smartregister.repository.TaskRepository;
 import org.smartregister.reveal.BaseUnitTest;
+import org.smartregister.reveal.R;
 import org.smartregister.reveal.model.TaskDetails;
 import org.smartregister.util.Cache;
 
 import java.util.UUID;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
@@ -28,6 +33,7 @@ import static org.powermock.api.mockito.PowerMockito.when;
 import static org.smartregister.domain.Task.TaskStatus.READY;
 import static org.smartregister.repository.BaseRepository.TYPE_Unsynced;
 import static org.smartregister.reveal.util.Constants.BusinessStatus.NOT_VISITED;
+import static org.smartregister.reveal.util.Constants.Intervention.BEDNET_DISTRIBUTION;
 import static org.smartregister.reveal.util.Constants.Intervention.CASE_CONFIRMATION;
 
 /**
@@ -48,27 +54,21 @@ public class TaskUtilsTests extends BaseUnitTest {
     @Captor
     private ArgumentCaptor<Task> taskArgumentCaptor;
 
+    private Context context;
+
     private TaskUtils taskUtils;
+
+    private String jurisdictionId;
+
 
     @Before
     public void setUp() {
+        context = RuntimeEnvironment.application;
+        jurisdictionId = UUID.randomUUID().toString();
         taskUtils = TaskUtils.getInstance();
         Whitebox.setInternalState(taskUtils, "taskRepository", taskRepository);
-    }
 
-    @Test
-    public void testResetTask() {
         String planId = UUID.randomUUID().toString();
-        String jurisdictionId = UUID.randomUUID().toString();
-        String taskIdentifier = UUID.randomUUID().toString();
-        TaskDetails taskDetails = TestingUtils.getTaskDetails();
-        taskDetails.setTaskCode(CASE_CONFIRMATION);
-
-        Task task = TestingUtils.getTask("task_1");
-        task.setCode(CASE_CONFIRMATION);
-        task.setIdentifier(taskIdentifier);
-
-
 
         when(prefsUtil.getCurrentPlanId()).thenReturn(planId);
         when(prefsUtil.getCurrentOperationalArea()).thenReturn(jurisdictionId);
@@ -77,6 +77,19 @@ public class TaskUtilsTests extends BaseUnitTest {
         Cache<Location> cache = mock(Cache.class);
         when(cache.get(anyString(), any())).thenReturn(jurisdiction);
         Whitebox.setInternalState(Utils.class, cache);
+
+    }
+
+    @Test
+    public void testResetTask() {
+
+        String taskIdentifier = UUID.randomUUID().toString();
+        TaskDetails taskDetails = TestingUtils.getTaskDetails();
+        taskDetails.setTaskCode(CASE_CONFIRMATION);
+
+        Task task = TestingUtils.getTask("task_1");
+        task.setCode(CASE_CONFIRMATION);
+        task.setIdentifier(taskIdentifier);
 
         when(taskRepository.getTaskByIdentifier(anyString())).thenReturn(task);
 
@@ -89,6 +102,31 @@ public class TaskUtilsTests extends BaseUnitTest {
         assertEquals(READY, taskArgumentCaptor.getValue().getStatus());
         assertEquals(TYPE_Unsynced, taskArgumentCaptor.getValue().getSyncStatus());
 
+
+    }
+
+    @Test
+    public void testGenerateTask() {
+
+        String expectedEntityId = UUID.randomUUID().toString();
+        String expectedStructureId = UUID.randomUUID().toString();
+        String expectedBusinessStatus = NOT_VISITED;
+        String expectedIntervention = BEDNET_DISTRIBUTION;
+        int expectedDescription = R.string.bednet_distribution_description;
+
+        Task actualTask = taskUtils.generateTask(context, expectedEntityId, expectedStructureId, expectedBusinessStatus, expectedIntervention, expectedDescription);
+
+        assertEquals(expectedEntityId, actualTask.getForEntity());
+        assertEquals(expectedStructureId, actualTask.getStructureId());
+        assertEquals(expectedIntervention, actualTask.getCode());
+        assertEquals(expectedBusinessStatus, actualTask.getBusinessStatus());
+        assertEquals(context.getString(expectedDescription), actualTask.getDescription());
+        assertNotNull(actualTask.getIdentifier());
+        assertEquals(3, actualTask.getPriority());
+        assertNotNull(actualTask.getExecutionStartDate());
+        assertNotNull(actualTask.getAuthoredOn());
+        assertNotNull(actualTask.getLastModified());
+        assertEquals(BaseRepository.TYPE_Created, actualTask.getSyncStatus());
 
     }
 }
