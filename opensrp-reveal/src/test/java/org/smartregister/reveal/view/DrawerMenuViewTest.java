e9ca8fa530feaeecb36fd86a8ac4d35e0392737d
richard <rkareko@ona.io>
2020-04-09 09:56:22 +0300

    Added DrawerMenuView tests

---
+++ opensrp-reveal/src/test/java/org/smartregister/reveal/view/DrawerMenuViewTest.java
@@ -0,0 +1,124 @@
+package org.smartregister.reveal.view;
+
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.widget.TextView;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import org.powermock.reflect.Whitebox;
+import org.smartregister.reveal.BaseUnitTest;
+import org.smartregister.reveal.contract.BaseDrawerContract;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by Richard Kareko on 4/9/20.
+ */
+
+public class DrawerMenuViewTest extends BaseUnitTest {
+
+    @Rule
+    public MockitoRule rule = MockitoJUnit.rule();
+
+    @Mock
+    private DrawerLayout mDrawerLayout;
+
+    @Mock
+    private BaseDrawerContract.DrawerActivity activity;
+
+    @Mock
+    private TextView planTextView;
+
+    @Mock
+    private TextView operationalAreaTextView;
+
+    @Captor
+    private ArgumentCaptor<String> stringArgumentCaptor;
+
+    private  DrawerMenuView drawerMenuView;
+
+    @Before
+    public void setUp() {
+        drawerMenuView = new DrawerMenuView(activity);
+    }
+
+    @Test
+    public void testDrawerMenuViewIsCreated() {
+        assertNotNull(drawerMenuView);
+        assertNotNull(Whitebox.getInternalState(drawerMenuView, "activity"));
+        assertNotNull(Whitebox.getInternalState(drawerMenuView, "presenter"));
+    }
+
+    @Test
+    public void testSetPlan() {
+        drawerMenuView = spy(drawerMenuView);
+        Whitebox.setInternalState(drawerMenuView, "planTextView", planTextView);
+
+        drawerMenuView.setPlan("FI plan");
+        verify(planTextView).setText(stringArgumentCaptor.capture());
+        assertEquals("FI plan", stringArgumentCaptor.getValue());
+    }
+
+    @Test
+    public void testGetPlan() {
+        drawerMenuView = spy(drawerMenuView);
+        Whitebox.setInternalState(drawerMenuView, "planTextView", planTextView);
+        when(planTextView.getText()).thenReturn("FI plan");
+
+        String actualPlan = drawerMenuView.getPlan();
+        assertEquals("FI plan", actualPlan);
+    }
+
+    @Test
+    public void testSetOperationalArea() {
+        drawerMenuView = spy(drawerMenuView);
+        Whitebox.setInternalState(drawerMenuView, "operationalAreaTextView", operationalAreaTextView);
+
+        drawerMenuView.setOperationalArea("Akros_1");
+        verify(operationalAreaTextView).setText(stringArgumentCaptor.capture());
+        assertEquals("Akros_1", stringArgumentCaptor.getValue());
+    }
+
+    @Test
+    public void testGetOperationalArea() {
+        drawerMenuView = spy(drawerMenuView);
+        Whitebox.setInternalState(drawerMenuView, "operationalAreaTextView", operationalAreaTextView);
+        when(operationalAreaTextView.getText()).thenReturn("Akros_1");
+
+        String actualOperationalArea = drawerMenuView.getOperationalArea();
+        assertEquals("Akros_1", actualOperationalArea);
+    }
+
+    @Test
+    public void testLockNavigationDrawerForSelection() {
+        drawerMenuView = spy(drawerMenuView);
+        Whitebox.setInternalState(drawerMenuView, "mDrawerLayout", mDrawerLayout);
+
+        drawerMenuView.lockNavigationDrawerForSelection();
+        verify(mDrawerLayout).openDrawer(GravityCompat.START);
+        verify(mDrawerLayout).setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_OPEN);
+    }
+
+    @Test
+    public void testUnlockNavigationDrawer() {
+        drawerMenuView = spy(drawerMenuView);
+        Whitebox.setInternalState(drawerMenuView, "mDrawerLayout", mDrawerLayout);
+        when(mDrawerLayout.getDrawerLockMode(GravityCompat.START)).thenReturn(DrawerLayout.LOCK_MODE_LOCKED_OPEN);
+
+        drawerMenuView.unlockNavigationDrawer();
+        verify(mDrawerLayout).closeDrawer(GravityCompat.START);
+        verify(mDrawerLayout).setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED);
+    }
+
+}
