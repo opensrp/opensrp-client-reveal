fc0d8378aeb57c87d7bfd42c4eaa62be476e799a
richard <rkareko@ona.io>
2020-04-09 12:43:00 +0300

    Added MDA  task grouping view holder tests

--- opensrp-reveal/src/test/java/org/smartregister/reveal/viewholder/TaskRegisterViewHolderTest.java
+++ opensrp-reveal/src/test/java/org/smartregister/reveal/viewholder/TaskRegisterViewHolderTest.java
@@ -26,6 +26,7 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.smartregister.reveal.util.Constants.Intervention.FI;
+import static org.smartregister.reveal.util.Constants.Intervention.MDA;
 
 /**
  * Created by samuelgithengi on 3/26/19.
@@ -236,9 +237,199 @@ public class TaskRegisterViewHolderTest extends BaseUnitTest {
 
     }
 
+    @Test
+    public void testSetTaskActionForMDATaskGroupingMDAAdhered() {
+        setMDAAsCurrentPlan();
+        when(taskDetails.getTaskCount()).thenReturn(2);
+        when(taskDetails.isFamilyRegistered()).thenReturn(true);
+        when(taskDetails.isMdaAdhered()).thenReturn(true);
+        viewHolder.setTaskAction("Record\n Status", taskDetails, cardDetails, onClickListener);
+        TextView taskDetails = viewHolder.itemView.findViewById(R.id.task_action);
+        assertEquals(View.VISIBLE, taskDetails.getVisibility());
+        assertEquals(context.getString(R.string.tasks_complete), taskDetails.getText());
+        assertEquals(context.getColor(R.color.text_black), taskDetails.getCurrentTextColor());
+        assertEquals(context.getResources().getDrawable(R.drawable.mda_adhered_bg).getConstantState(), taskDetails.getBackground().getConstantState());
+        taskDetails.performClick();
+        verify(onClickListener).onClick(taskDetails);
+
+    }
+
+    @Test
+    public void testSetTaskActionForMDATaskGroupingMdaDispenced() {
+        setMDAAsCurrentPlan();
+        when(taskDetails.getTaskCount()).thenReturn(2);
+        when(taskDetails.isFamilyRegistered()).thenReturn(true);
+        when(taskDetails.isMdaAdhered()).thenReturn(false);
+        when(taskDetails.isFullyReceived()).thenReturn(true);
+        viewHolder.setTaskAction("Record\n Status", taskDetails, cardDetails, onClickListener);
+        TextView taskDetails = viewHolder.itemView.findViewById(R.id.task_action);
+        assertEquals(View.VISIBLE, taskDetails.getVisibility());
+        assertEquals(context.getString(R.string.view_tasks), taskDetails.getText());
+        assertEquals(context.getColor(R.color.text_black), taskDetails.getCurrentTextColor());
+        assertEquals(context.getResources().getDrawable(R.drawable.mda_dispensed_bg).getConstantState(), taskDetails.getBackground().getConstantState());
+        taskDetails.performClick();
+        verify(onClickListener).onClick(taskDetails);
+
+    }
+
+    @Test
+    public void testSetTaskActionForMDATaskGroupingPartiallyReceived() {
+        setMDAAsCurrentPlan();
+        when(taskDetails.getTaskCount()).thenReturn(2);
+        when(taskDetails.isFamilyRegistered()).thenReturn(true);
+        when(taskDetails.isMdaAdhered()).thenReturn(false);
+        when(taskDetails.isFullyReceived()).thenReturn(false);
+        when(taskDetails.isPartiallyReceived()).thenReturn(true);
+        viewHolder.setTaskAction("Record\n Status", taskDetails, cardDetails, onClickListener);
+        TextView taskDetails = viewHolder.itemView.findViewById(R.id.task_action);
+        assertEquals(View.VISIBLE, taskDetails.getVisibility());
+        assertEquals(context.getString(R.string.view_tasks), taskDetails.getText());
+        assertEquals(context.getColor(R.color.text_black), taskDetails.getCurrentTextColor());
+        assertEquals(context.getResources().getDrawable(R.drawable.mda_partially_received_bg).getConstantState(), taskDetails.getBackground().getConstantState());
+        taskDetails.performClick();
+        verify(onClickListener).onClick(taskDetails);
+
+    }
+
+    @Test
+    public void testSetTaskActionForMDATaskGroupingNoneReceived() {
+        setMDAAsCurrentPlan();
+        when(taskDetails.getTaskCount()).thenReturn(2);
+        when(taskDetails.isFamilyRegistered()).thenReturn(true);
+        when(taskDetails.isMdaAdhered()).thenReturn(false);
+        when(taskDetails.isFullyReceived()).thenReturn(false);
+        when(taskDetails.isPartiallyReceived()).thenReturn(false);
+        when(taskDetails.isNoneReceived()).thenReturn(true);
+        viewHolder.setTaskAction("Record\n Status", taskDetails, cardDetails, onClickListener);
+        TextView taskDetails = viewHolder.itemView.findViewById(R.id.task_action);
+        assertEquals(View.VISIBLE, taskDetails.getVisibility());
+        assertEquals(context.getString(R.string.view_tasks), taskDetails.getText());
+        assertEquals(context.getColor(R.color.text_black), taskDetails.getCurrentTextColor());
+        assertEquals(context.getResources().getDrawable(R.drawable.mda_none_received_bg).getConstantState(), taskDetails.getBackground().getConstantState());
+        taskDetails.performClick();
+        verify(onClickListener).onClick(taskDetails);
+
+    }
+
+    @Test
+    public void testSetTaskActionForMDATaskGroupingNotEligible() {
+        setMDAAsCurrentPlan();
+        when(taskDetails.getTaskCount()).thenReturn(2);
+        when(taskDetails.isFamilyRegistered()).thenReturn(true);
+        when(taskDetails.isMdaAdhered()).thenReturn(false);
+        when(taskDetails.isFullyReceived()).thenReturn(false);
+        when(taskDetails.isPartiallyReceived()).thenReturn(false);
+        when(taskDetails.isNoneReceived()).thenReturn(false);
+        when(taskDetails.isNotEligible()).thenReturn(true);
+        viewHolder.setTaskAction("Record\n Status", taskDetails, cardDetails, onClickListener);
+        TextView taskDetails = viewHolder.itemView.findViewById(R.id.task_action);
+        assertEquals(View.VISIBLE, taskDetails.getVisibility());
+        assertEquals(context.getString(R.string.view_tasks), taskDetails.getText());
+        assertEquals(context.getColor(R.color.text_black), taskDetails.getCurrentTextColor());
+        assertEquals(context.getResources().getDrawable(R.drawable.mda_not_eligible_bg).getConstantState(), taskDetails.getBackground().getConstantState());
+        taskDetails.performClick();
+        verify(onClickListener).onClick(taskDetails);
+
+    }
+
+    @Test
+    public void testSetTaskActionForMDATaskGroupingFamRegistered() {
+        setMDAAsCurrentPlan();
+        when(taskDetails.getTaskCount()).thenReturn(2);
+        when(taskDetails.isFamilyRegistered()).thenReturn(true);
+        when(taskDetails.isMdaAdhered()).thenReturn(false);
+        when(taskDetails.isFullyReceived()).thenReturn(false);
+        when(taskDetails.isPartiallyReceived()).thenReturn(false);
+        when(taskDetails.isNoneReceived()).thenReturn(false);
+        when(taskDetails.isNotEligible()).thenReturn(false);
+        viewHolder.setTaskAction("Record\n Status", taskDetails, cardDetails, onClickListener);
+        TextView taskDetails = viewHolder.itemView.findViewById(R.id.task_action);
+        assertEquals(View.VISIBLE, taskDetails.getVisibility());
+        assertEquals(context.getString(R.string.view_tasks), taskDetails.getText());
+        assertEquals(context.getColor(R.color.text_black), taskDetails.getCurrentTextColor());
+        assertEquals(context.getResources().getDrawable(R.drawable.family_registered_bg).getConstantState(), taskDetails.getBackground().getConstantState());
+        taskDetails.performClick();
+        verify(onClickListener).onClick(taskDetails);
+
+    }
+
+    @Test
+    public void testSetTaskActionForMDATaskGroupingNoneComplete() {
+        setMDAAsCurrentPlan();
+        when(taskDetails.getTaskCount()).thenReturn(2);
+        when(taskDetails.isFamilyRegTaskExists()).thenReturn(true);
+        when(taskDetails.isFamilyRegistered()).thenReturn(false);
+        when(taskDetails.isMdaAdhered()).thenReturn(false);
+        when(taskDetails.isFullyReceived()).thenReturn(false);
+        when(taskDetails.isPartiallyReceived()).thenReturn(false);
+        when(taskDetails.isNoneReceived()).thenReturn(false);
+        when(taskDetails.isNotEligible()).thenReturn(false);
+        viewHolder.setTaskAction("Record\n Status", taskDetails, cardDetails, onClickListener);
+        TextView taskDetails = viewHolder.itemView.findViewById(R.id.task_action);
+        assertEquals(View.VISIBLE, taskDetails.getVisibility());
+        assertEquals(context.getString(R.string.view_tasks), taskDetails.getText());
+        assertEquals(context.getColor(R.color.text_black), taskDetails.getCurrentTextColor());
+        assertEquals(context.getResources().getDrawable(R.drawable.no_task_complete_bg).getConstantState(), taskDetails.getBackground().getConstantState());
+        taskDetails.performClick();
+        verify(onClickListener).onClick(taskDetails);
+
+    }
+
+    @Test
+    public void testShowFITasksCompleteActionView() {
+        setFIAsCurrentPlan();
+        when(taskDetails.getTaskCount()).thenReturn(2);
+        when(taskDetails.getCompleteTaskCount()).thenReturn(2);
+        viewHolder.setTaskAction("Record\n Status", taskDetails, cardDetails, onClickListener);
+        TextView taskDetails = viewHolder.itemView.findViewById(R.id.task_action);
+        assertEquals(View.VISIBLE, taskDetails.getVisibility());
+        assertEquals(context.getString(R.string.tasks_complete), taskDetails.getText());
+        assertEquals(context.getColor(R.color.text_black), taskDetails.getCurrentTextColor());
+        assertEquals(context.getResources().getDrawable(R.drawable.tasks_complete_bg).getConstantState(), taskDetails.getBackground().getConstantState());
+        taskDetails.performClick();
+        verify(onClickListener).onClick(taskDetails);
+
+    }
+
+    @Test
+    public void testShowMDATasksCompleteActionView() {
+        setMDAAsCurrentPlan();
+        when(taskDetails.getTaskCount()).thenReturn(2);
+        when(taskDetails.getCompleteTaskCount()).thenReturn(2);
+        viewHolder.setTaskAction("Record\n Status", taskDetails, cardDetails, onClickListener);
+        TextView taskDetails = viewHolder.itemView.findViewById(R.id.task_action);
+        assertEquals(View.VISIBLE, taskDetails.getVisibility());
+        assertEquals(context.getString(R.string.tasks_complete), taskDetails.getText());
+        assertEquals(context.getColor(R.color.text_black), taskDetails.getCurrentTextColor());
+        assertEquals(context.getResources().getDrawable(R.drawable.mda_adhered_bg).getConstantState(), taskDetails.getBackground().getConstantState());
+        taskDetails.performClick();
+        verify(onClickListener).onClick(taskDetails);
+
+    }
+
+    @Test
+    public void testSetHouseNumber() {
+        viewHolder.setHouseNumber("House 2");
+        TextView taskDetails = viewHolder.itemView.findViewById(R.id.house_number);
+        assertEquals("House 2", taskDetails.getText());
+    }
+
+
+    @Test
+    public void testShowHouseNumber() {
+        viewHolder.showHouseNumber();
+        TextView taskDetails = viewHolder.itemView.findViewById(R.id.house_number);
+        assertEquals(View.VISIBLE, taskDetails.getVisibility());
+    }
+
     private void setFIAsCurrentPlan() {
         PreferencesUtil.getInstance().setCurrentPlan("Focus 1");
         PreferencesUtil.getInstance().setInterventionTypeForPlan("Focus 1", FI);
+    }
+
+    private void setMDAAsCurrentPlan() {
+        PreferencesUtil.getInstance().setCurrentPlan("MDA 1");
+        PreferencesUtil.getInstance().setInterventionTypeForPlan("MDA 1", MDA);
     }
 
 }
