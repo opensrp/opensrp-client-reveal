6b0a5b96ff56fe6c16f9a83a044a194681e25760
richard <rkareko@ona.io>
2020-04-21 16:48:47 +0300

    Add patner logo to bottom right for thailand apk

--- opensrp-reveal/src/main/java/org/smartregister/reveal/activity/LoginActivity.java
+++ opensrp-reveal/src/main/java/org/smartregister/reveal/activity/LoginActivity.java
@@ -1,10 +1,16 @@
 package org.smartregister.reveal.activity;
 
 import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.ImageView;
 
+import org.smartregister.reveal.BuildConfig;
 import org.smartregister.reveal.R;
 import org.smartregister.reveal.application.RevealApplication;
 import org.smartregister.reveal.presenter.LoginPresenter;
+import org.smartregister.reveal.util.Country;
 import org.smartregister.reveal.view.ListTasksActivity;
 import org.smartregister.task.SaveTeamLocationsTask;
 import org.smartregister.view.activity.BaseLoginActivity;
@@ -45,4 +51,12 @@ public class LoginActivity extends BaseLoginActivity implements BaseLoginContrac
         }
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (BuildConfig.BUILD_COUNTRY == Country.THAILAND || BuildConfig.BUILD_COUNTRY == Country.THAILAND_EN) {
+            ImageView bvbdLogo = findViewById(R.id.partner_logo);
+            bvbdLogo.setVisibility(View.VISIBLE);
+        }
+    }
 }
