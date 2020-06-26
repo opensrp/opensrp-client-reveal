0983a776ade8552bd39c0a5555cb2493a463e265
rkodev <43806892+rkodev@users.noreply.github.com>
2020-06-26 16:47:08 +0300

    Fix AndoridX migration

--- opensrp-reveal/src/test/java/org/smartregister/reveal/fragment/ChildRegisterFragmentTest.java
+++ opensrp-reveal/src/test/java/org/smartregister/reveal/fragment/ChildRegisterFragmentTest.java
@@ -2,9 +2,10 @@ package org.smartregister.reveal.fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.TextView;
+
+import androidx.fragment.app.FragmentActivity;
 
 import org.hamcrest.CoreMatchers;
 import org.json.JSONException;
