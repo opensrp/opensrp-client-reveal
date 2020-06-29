b6d0d040aa51e727d34e1f8a928eda57eea72aac
Samuel Githengi <samuel.githengi@gmail.com>
2020-06-29 17:58:54 +0300

    Trigger plan evaluation

--- opensrp-reveal/src/main/java/org/smartregister/reveal/sync/RevealClientProcessor.java
+++ opensrp-reveal/src/main/java/org/smartregister/reveal/sync/RevealClientProcessor.java
@@ -97,6 +97,7 @@ public class RevealClientProcessor extends ClientProcessorForJava {
         processClient(eventClientList, false);
     }
 
+    @Override
     public void processClient(List<EventClient> eventClients, boolean localEvents) {
         ClientClassification clientClassification = assetJsonToJava("ec_client_classification.json", ClientClassification.class);
         if (clientClassification == null) {
@@ -152,6 +153,10 @@ public class RevealClientProcessor extends ClientProcessorForJava {
                 if (!hasSyncedEventsInTarget && operationalAreaLocationId != null &&
                         operationalAreaLocationId.equals(operationalAreaId)) {
                     hasSyncedEventsInTarget = true;
+                }
+
+                if (localEvents) {
+                    processPlanEvaluation(eventClient);
                 }
             }
         }
