1c947412ed4691c3fc5ab47dd5fd5b280eb48125
Samuel Githengi <samuel.githengi@gmail.com>
2020-07-01 09:58:48 +0300

    Delete calls to generate tasks

--- opensrp-reveal/src/main/java/org/smartregister/reveal/util/TaskUtils.java
+++ opensrp-reveal/src/main/java/org/smartregister/reveal/util/TaskUtils.java
@@ -1,32 +1,22 @@
 package org.smartregister.reveal.util;
 
-import android.content.Context;
-
-import androidx.annotation.StringRes;
-
 import net.sqlcipher.Cursor;
 import net.sqlcipher.database.SQLiteDatabase;
 
 import org.joda.time.DateTime;
 import org.smartregister.clientandeventmodel.Event;
-import org.smartregister.domain.Action;
-import org.smartregister.domain.Period;
-import org.smartregister.domain.PlanDefinition;
 import org.smartregister.domain.Task;
 import org.smartregister.repository.AllSharedPreferences;
 import org.smartregister.repository.BaseRepository;
 import org.smartregister.repository.PlanDefinitionRepository;
 import org.smartregister.repository.TaskRepository;
 import org.smartregister.reveal.BuildConfig;
-import org.smartregister.reveal.R;
 import org.smartregister.reveal.application.RevealApplication;
 import org.smartregister.reveal.model.BaseTaskDetails;
 import org.smartregister.reveal.util.Constants.BusinessStatus;
 import org.smartregister.reveal.util.Constants.Intervention;
 
 import java.util.List;
-import java.util.Set;
-import java.util.UUID;
 
 import timber.log.Timber;
 
@@ -66,76 +56,6 @@ public class TaskUtils {
         prefsUtil = PreferencesUtil.getInstance();
         planRepository = RevealApplication.getInstance().getPlanDefinitionRepository();
         revealApplication = RevealApplication.getInstance();
-    }
-
-    public void generateBloodScreeningTask(Context context, String entityId, String structureId) {
-        if (planActionCodesContainIntervention(prefsUtil.getCurrentPlanId(), Intervention.BLOOD_SCREENING)) {
-            generateTask(context, entityId, structureId, BusinessStatus.NOT_VISITED, Intervention.BLOOD_SCREENING,
-                    R.string.blood_screening_description);
-        }
-
-    }
-
-    public void generateBedNetDistributionTask(Context context, String entityId) {
-        if (planActionCodesContainIntervention(prefsUtil.getCurrentPlanId(), Intervention.BEDNET_DISTRIBUTION)) {
-            generateTask(context, entityId, entityId, BusinessStatus.NOT_VISITED, Intervention.BEDNET_DISTRIBUTION,
-                    R.string.bednet_distribution_description);
-        }
-    }
-
-    public Task generateTask(Context context, String entityId, String structureId, String businessStatus, String intervention, @StringRes int description) {
-        Task task = new Task();
-        DateTime now = new DateTime();
-        task.setIdentifier(UUID.randomUUID().toString());
-        task.setPlanIdentifier(prefsUtil.getCurrentPlanId());
-        task.setGroupIdentifier(Utils.getOperationalAreaLocation(prefsUtil.getCurrentOperationalArea()).getId());
-        task.setStatus(READY);
-        task.setBusinessStatus(businessStatus);
-        task.setPriority(Task.TaskPriority.ROUTINE);
-        task.setCode(intervention);
-        task.setDescription(context.getString(description));
-        PlanDefinition currentPlan = planRepository.findPlanDefinitionById(prefsUtil.getCurrentPlanId());
-        if (currentPlan != null && currentPlan.getActions() != null) {
-            for (Action action : currentPlan.getActions()) {
-                if (intervention.equals(action.getCode())) {
-                    task.setFocus(action.getIdentifier());
-                    continue;
-                }
-            }
-        }
-        task.setForEntity(entityId);
-        task.setStructureId(structureId);
-        Period period= new Period();
-        period.setStart(now);
-        task.setExecutionPeriod(period);
-        task.setAuthoredOn(now);
-        task.setLastModified(now);
-        task.setOwner(sharedPreferences.fetchRegisteredANM());
-        task.setSyncStatus(BaseRepository.TYPE_Created);
-        taskRepository.addOrUpdate(task);
-        revealApplication.setSynced(false);
-        return task;
-    }
-
-
-    public Task generateRegisterFamilyTask(Context context, String entityId) {
-        return generateTask(context, entityId, entityId, BusinessStatus.NOT_VISITED, Intervention.REGISTER_FAMILY,
-                R.string.register_family_description);
-
-    }
-
-    public void generateMDADispenseTask(Context context, String entityId, String structureId) {
-        generateTask(context, entityId, structureId, BusinessStatus.NOT_VISITED, Intervention.MDA_DISPENSE,
-                R.string.mda_dispense_desciption);
-    }
-
-    public void generateMDAAdherenceTask(Context context, String entityId, String structureId) {
-
-        Set<Task> tasks = taskRepository.getTasksByEntityAndCode(prefsUtil.getCurrentPlanId(), Utils.getOperationalAreaLocation(prefsUtil.getCurrentOperationalArea()).getId(), entityId, Intervention.MDA_ADHERENCE);
-        if (tasks == null || tasks.isEmpty()) {
-            generateTask(context, entityId, structureId, BusinessStatus.NOT_VISITED, Intervention.MDA_ADHERENCE,
-                    R.string.mda_adherence_desciption);
-        }
     }
 
     public void tagEventTaskDetails(List<Event> events, SQLiteDatabase sqLiteDatabase) {
