c858d2164674b7470480789667feccbb17dcd03d
richard <rkareko@ona.io>
2020-07-09 19:06:32 +0300

    Handle load image result in else statement

--- opensrp-reveal/src/main/java/org/smartregister/reveal/activity/RevealJsonFormActivity.java
+++ opensrp-reveal/src/main/java/org/smartregister/reveal/activity/RevealJsonFormActivity.java
@@ -45,7 +45,7 @@ public class RevealJsonFormActivity extends FormConfigurationJsonFormActivity im
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+
         if (requestCode == Constants.RequestCode.LOCATION_SETTINGS && requestedLocation) {
             if (resultCode == RESULT_OK) {
                 formFragment.getPresenter().getLocationUtils().requestLocationUpdates(formFragment.getPresenter().getLocationListener());
@@ -54,6 +54,8 @@ public class RevealJsonFormActivity extends FormConfigurationJsonFormActivity im
                 formFragment.getPresenter().getLocationPresenter().onGetUserLocationFailed();
             }
             requestedLocation = false;
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
         }
     }
 
