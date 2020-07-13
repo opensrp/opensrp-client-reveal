36ed7ac47051f7d7555ca2e9828bbf86bae19937
Samuel Githengi <samuel.githengi@gmail.com>
2020-07-13 19:20:38 +0300

    Add client relationship table

--- opensrp-reveal/src/main/java/org/smartregister/reveal/repository/RevealRepository.java
+++ opensrp-reveal/src/main/java/org/smartregister/reveal/repository/RevealRepository.java
@@ -7,6 +7,7 @@ import net.sqlcipher.database.SQLiteDatabase;
 import org.apache.commons.lang3.StringUtils;
 import org.smartregister.AllConstants;
 import org.smartregister.configurableviews.repository.ConfigurableViewsRepository;
+import org.smartregister.domain.ClientRelationship;
 import org.smartregister.domain.db.EventClient;
 import org.smartregister.job.PullUniqueIdsServiceJob;
 import org.smartregister.repository.BaseRepository;
@@ -90,6 +91,7 @@ public class RevealRepository extends Repository {
         TaskRepository.createTable(database);
         LocationRepository.createTable(database);
         StructureRepository.createTable(database);
+        ClientRelationshipRepository.createTable(database);
 
         onUpgrade(database, 1, BuildConfig.DATABASE_VERSION);
     }
