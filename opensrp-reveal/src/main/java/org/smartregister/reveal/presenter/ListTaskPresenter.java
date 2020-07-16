7cc0dfc995b1bfe028c2c1dfd1669270c9e27909
richard <rkareko@ona.io>
2020-07-16 13:24:30 +0300

    Populate spray form with event data when editting

--- opensrp-reveal/src/main/java/org/smartregister/reveal/presenter/ListTaskPresenter.java
+++ opensrp-reveal/src/main/java/org/smartregister/reveal/presenter/ListTaskPresenter.java
@@ -20,6 +20,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.smartregister.commonregistry.CommonPersonObjectClient;
+import org.smartregister.domain.Event;
 import org.smartregister.domain.Task;
 import org.smartregister.domain.Task.TaskStatus;
 import org.smartregister.reveal.BuildConfig;
@@ -365,6 +366,16 @@ public class ListTaskPresenter implements ListTaskContract.Presenter, PasswordRe
     }
 
     @Override
+    public void onEventFound(Event event) {
+        startForm(selectedFeature, cardDetails, selectedFeatureInterventionType, event);
+    }
+
+    @Override
+    public void findLastEvent(String featureId, String eventType) {
+        listTaskInteractor.findLastEvent(featureId, eventType);
+    }
+
+    @Override
     public void onInterventionFormDetailsFetched(CardDetails cardDetails) {
         this.cardDetails = cardDetails;
         this.changeInterventionStatus = true;
@@ -434,18 +445,20 @@ public class ListTaskPresenter implements ListTaskContract.Presenter, PasswordRe
         familyCardDetails.setDateCreated(formatDate(originalDate));
     }
 
-
-    public void startForm(Feature feature, CardDetails cardDetails, String interventionType) {
+    public void startForm(Feature feature, CardDetails cardDetails, String interventionType){
+        startForm(feature, cardDetails, interventionType, null);
+    }
+    public void startForm(Feature feature, CardDetails cardDetails, String interventionType, Event event) {
         String formName = jsonFormUtils.getFormName(null, interventionType);
         String sprayStatus = cardDetails == null ? null : cardDetails.getStatus();
         String familyHead = null;
         if (cardDetails instanceof SprayCardDetails) {
             familyHead = ((SprayCardDetails) cardDetails).getFamilyHead();
         }
-        startForm(formName, feature, sprayStatus, familyHead);
+        startForm(formName, feature, sprayStatus, familyHead, event);
     }
 
-    private void startForm(String formName, Feature feature, String sprayStatus, String familyHead) {
+    private void startForm(String formName, Feature feature, String sprayStatus, String familyHead, Event event) {
         JSONObject formJson = jsonFormUtils.getFormJSON(listTaskView.getContext()
                 , formName, feature, sprayStatus, familyHead);
         if (cardDetails instanceof MosquitoHarvestCardDetails && PAOT.equals(((MosquitoHarvestCardDetails) cardDetails).getInterventionType())) {
@@ -462,6 +475,7 @@ public class ListTaskPresenter implements ListTaskContract.Presenter, PasswordRe
             jsonFormUtils.populateServerOptions(RevealApplication.getInstance().getServerConfigs(), formJson, CONFIGURATION.DATA_COLLECTORS, JsonForm.DATA_COLLECTOR, prefsUtil.getCurrentDistrict());
             jsonFormUtils.populateServerOptions(RevealApplication.getInstance().getServerConfigs(), formJson, CONFIGURATION.HEALTH_FACILITIES, JsonForm.HFC_SEEK, prefsUtil.getCurrentDistrict());
             jsonFormUtils.populateServerOptions(RevealApplication.getInstance().getServerConfigs(), formJson, CONFIGURATION.HEALTH_FACILITIES, JsonForm.HFC_BELONG, prefsUtil.getCurrentDistrict());
+            jsonFormUtils.populateForm(event, formJson);
         } else if (JsonForm.SPRAY_FORM_REFAPP.equals(formName)) {
             jsonFormUtils.populateServerOptions(RevealApplication.getInstance().getServerConfigs(), formJson, CONFIGURATION.DATA_COLLECTORS, JsonForm.DATA_COLLECTOR, prefsUtil.getCurrentDistrict());
         }
@@ -571,7 +585,11 @@ public class ListTaskPresenter implements ListTaskContract.Presenter, PasswordRe
         } else if (cardDetails == null || !changeInterventionStatus) {
             startForm(selectedFeature, null, selectedFeatureInterventionType);
         } else {
-            startForm(selectedFeature, cardDetails, selectedFeatureInterventionType);
+            if (IRS.equals(cardDetails.getInterventionType())) {
+                findLastEvent(selectedFeature.id(), SPRAY_EVENT);
+            } else {
+                startForm(selectedFeature, cardDetails, selectedFeatureInterventionType);
+            }
         }
     }
 
