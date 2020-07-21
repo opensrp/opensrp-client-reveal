b0701329bbbc5e06305e68cbe41d90bd70e6c65c
Samuel Githengi <samuel.githengi@gmail.com>
2020-07-21 20:44:34 +0300

    set core version to version 2.0.0

--- opensrp-reveal/src/main/java/org/smartregister/reveal/view/TaskRegisterActivity.java
+++ opensrp-reveal/src/main/java/org/smartregister/reveal/view/TaskRegisterActivity.java
@@ -1,10 +1,10 @@
 package org.smartregister.reveal.view;
 
 import android.content.Intent;
+import android.view.View;
+
 import androidx.annotation.VisibleForTesting;
 import androidx.fragment.app.Fragment;
-
-import android.view.View;
 
 import org.json.JSONObject;
 import org.smartregister.reveal.R;
@@ -59,12 +59,7 @@ public class TaskRegisterActivity extends BaseRegisterActivity implements BaseRe
     }
 
     @Override
-    public void startFormActivity(String s, String s1, Map<String, String> map) {
-
-    }
-
-    @Override
-    public void startFormActivity(String formName, String entityId, String metaData) {//not used
+    public void startFormActivity(String s, String s1, Map<String, String> map) {//not used
     }
 
     @Override
