6463735d0dbb26bcaf3f37b86c9560c8b08fae69
richard <rkareko@ona.io>
2020-08-24 12:23:49 +0300

    Added larval dipping and mosquito collection forms for Thailand EN build

--- opensrp-reveal/src/main/java/org/smartregister/reveal/util/Constants.java
+++ opensrp-reveal/src/main/java/org/smartregister/reveal/util/Constants.java
@@ -333,7 +333,11 @@ public interface Constants {
 
         String THAILAND_LARVAL_DIPPING_FORM = "json.form/thailand_larval_dipping_form.json";
 
+        String THAILAND_EN_LARVAL_DIPPING_FORM = "json.form/thailand_en_larval_dipping_form.json";
+
         String THAILAND_MOSQUITO_COLLECTION_FORM = "json.form/thailand_mosquito_collection_form.json";
+
+        String THAILAND_EN_MOSQUITO_COLLECTION_FORM = "json.form/thailand_en_mosquito_collection_form.json";
 
         String THAILAND_SPRAY_FORM = "json.form/thailand_spray_form.json";
 
--- opensrp-reveal/src/main/java/org/smartregister/reveal/util/RevealJsonFormUtils.java
+++ opensrp-reveal/src/main/java/org/smartregister/reveal/util/RevealJsonFormUtils.java
@@ -248,6 +248,8 @@ public class RevealJsonFormUtils {
                 || Intervention.MOSQUITO_COLLECTION.equals(taskCode)) {
             if (BuildConfig.BUILD_COUNTRY == Country.THAILAND) {
                 formName = JsonForm.THAILAND_MOSQUITO_COLLECTION_FORM;
+            } else if (BuildConfig.BUILD_COUNTRY == Country.THAILAND_EN) {
+                formName = JsonForm.THAILAND_EN_MOSQUITO_COLLECTION_FORM;
             } else if (BuildConfig.BUILD_COUNTRY == Country.REFAPP) {
                 formName = JsonForm.REFAPP_MOSQUITO_COLLECTION_FORM;
             } else {
@@ -283,6 +285,8 @@ public class RevealJsonFormUtils {
         } else if (LARVAL_DIPPING_EVENT.equals(encounterType) || Intervention.LARVAL_DIPPING.equals(taskCode)) {
             if (BuildConfig.BUILD_COUNTRY == Country.THAILAND) {
                 formName = JsonForm.THAILAND_LARVAL_DIPPING_FORM;
+            } else if (BuildConfig.BUILD_COUNTRY == Country.THAILAND_EN) {
+                formName = JsonForm.THAILAND_EN_LARVAL_DIPPING_FORM;
             } else if (BuildConfig.BUILD_COUNTRY == Country.REFAPP) {
                 formName = JsonForm.REFAPP_LARVAL_DIPPING_FORM;
             } else {
