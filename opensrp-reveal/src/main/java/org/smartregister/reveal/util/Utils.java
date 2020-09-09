7e47252edac8069dff01602b36e7c08b6240f848
richard <rkareko@ona.io>
2020-09-09 14:24:40 +0300

    Code clean up

--- opensrp-reveal/src/main/java/org/smartregister/reveal/util/Utils.java
+++ opensrp-reveal/src/main/java/org/smartregister/reveal/util/Utils.java
@@ -425,12 +425,12 @@ public class Utils {
         JSONArray updatedCoords = null;
         try {
             if (originalGeometry instanceof  MultiPolygon) {
-                MultiPolygon editedGeometryMultiPolygon = MultiPolygon.fromPolygons(Collections.singletonList((Polygon) updatedGeometry));
+                MultiPolygon editedGeometryMultiPolygon = MultiPolygon.fromPolygon((Polygon) updatedGeometry);;
                 editedGeometryJson = new JSONObject(editedGeometryMultiPolygon.toJson());
             } else {
                 editedGeometryJson = new JSONObject(updatedGeometry.toJson());
             }
-            updatedCoords = (JSONArray) editedGeometryJson.get("coordinates");
+            updatedCoords = editedGeometryJson.getJSONArray("coordinates");;
         } catch (JSONException e) {
             Timber.e(e);
         }
