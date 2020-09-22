4ddba856c215f9299691a3077faf35f89e5df10b
richard <rkareko@ona.io>
2020-09-22 11:42:44 +0300

    Added Location Picker fragment, presenter and interactor

---
+++ opensrp-reveal/src/main/java/org/smartregister/reveal/fragment/LocationPickerFragment.java
@@ -0,0 +1,36 @@
+package org.smartregister.reveal.fragment;
+
+import android.view.View;
+
+import org.smartregister.domain.Location;
+import org.smartregister.reveal.contract.LocationPickerFragmentContract;
+
+import java.util.List;
+
+/**
+ * Created by Richard Kareko on 9/22/20.
+ */
+
+public class LocationPickerFragment implements LocationPickerFragmentContract.View, View.OnClickListener {
+
+
+    @Override
+    public void disableCheckBox(String locationId) {
+
+    }
+
+    @Override
+    public void enableCheckBox(String locationId) {
+
+    }
+
+    @Override
+    public void setAvailableLocations(List<Location> locations) {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
