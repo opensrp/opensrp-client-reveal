fd0c5cc1401ead654656193419146a4859273ddc
richard <rkareko@ona.io>
2020-11-03 15:09:59 +0300

    Populate location dropdowns with districts for Macepa forms

--- opensrp-reveal/src/main/java/org/smartregister/reveal/util/Constants.java
+++ opensrp-reveal/src/main/java/org/smartregister/reveal/util/Constants.java
@@ -66,6 +66,8 @@ public interface Constants {
 
     String DG_ID_PLACEHOLDER = "DIGITAL_GLOBE_ID";
 
+    List<String> MACEPA_PROVINCES = Arrays.asList("Western", "Southern", "Lusaka");
+
     interface CONFIGURATION {
         String LOGIN = "login";
         String GLOBAL_CONFIGS = "global_configs";
--- opensrp-reveal/src/main/java/org/smartregister/reveal/util/RevealJsonFormUtils.java
+++ opensrp-reveal/src/main/java/org/smartregister/reveal/util/RevealJsonFormUtils.java
@@ -62,10 +62,12 @@ import static org.smartregister.reveal.util.Constants.JSON_FORM_PARAM_JSON;
 import static org.smartregister.reveal.util.Constants.JsonForm.JSON_FORM_FOLDER;
 import static org.smartregister.reveal.util.Constants.JsonForm.YES;
 import static org.smartregister.reveal.util.Constants.LARVAL_DIPPING_EVENT;
+import static org.smartregister.reveal.util.Constants.MACEPA_PROVINCES;
 import static org.smartregister.reveal.util.Constants.MOSQUITO_COLLECTION_EVENT;
 import static org.smartregister.reveal.util.Constants.REGISTER_STRUCTURE_EVENT;
 import static org.smartregister.reveal.util.Constants.RequestCode.REQUEST_CODE_GET_JSON;
 import static org.smartregister.reveal.util.Constants.SPRAY_EVENT;
+import static org.smartregister.reveal.util.Constants.Tags.DISTRICT;
 import static org.smartregister.reveal.util.Constants.Tags.OPERATIONAL_AREA;
 import static org.smartregister.reveal.util.Constants.Tags.ZONE;
 import static org.smartregister.reveal.util.Utils.getPropertyValue;
@@ -522,7 +524,11 @@ public class RevealJsonFormUtils {
                             dataCollector);
                 }
 
-                populateUserAssignedLocations(formJSON, JsonForm.ZONE, Arrays.asList(OPERATIONAL_AREA, ZONE));
+                if (MACEPA_PROVINCES.contains(PreferencesUtil.getInstance().getCurrentProvince())) {
+                    populateUserAssignedLocations(formJSON, JsonForm.ZONE, Arrays.asList(DISTRICT));
+                } else {
+                    populateUserAssignedLocations(formJSON, JsonForm.ZONE, Arrays.asList(OPERATIONAL_AREA, ZONE));
+                }
                 break;
 
             case JsonForm.TEAM_LEADER_DOS_ZAMBIA:
@@ -538,7 +544,11 @@ public class RevealJsonFormUtils {
                             dataCollector.split(":")[0]);
                 }
 
-                populateUserAssignedLocations(formJSON, JsonForm.ZONE, Arrays.asList(OPERATIONAL_AREA, ZONE));
+                if (MACEPA_PROVINCES.contains(PreferencesUtil.getInstance().getCurrentProvince())) {
+                    populateUserAssignedLocations(formJSON, JsonForm.ZONE, Arrays.asList(DISTRICT));
+                } else {
+                    populateUserAssignedLocations(formJSON, JsonForm.ZONE, Arrays.asList(OPERATIONAL_AREA, ZONE));
+                }
 
                 break;
 
