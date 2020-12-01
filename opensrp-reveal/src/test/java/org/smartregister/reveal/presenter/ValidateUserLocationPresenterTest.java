500eb0bfc0621ca084ed795cb2e188201ad9b07f
Samuel Githengi <samuel.githengi@gmail.com>
2020-12-01 11:40:48 +0300

    Unit test wait fro location

--- opensrp-reveal/src/test/java/org/smartregister/reveal/presenter/ValidateUserLocationPresenterTest.java
+++ opensrp-reveal/src/test/java/org/smartregister/reveal/presenter/ValidateUserLocationPresenterTest.java
@@ -7,9 +7,16 @@ import com.mapbox.mapboxsdk.geometry.LatLng;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.robolectric.util.ReflectionHelpers;
+import org.robolectric.util.ReflectionHelpers.ClassParameter;
 import org.smartregister.reveal.BaseUnitTest;
+import org.smartregister.reveal.R;
 import org.smartregister.reveal.contract.UserLocationContract;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.timeout;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -26,9 +33,14 @@ public class ValidateUserLocationPresenterTest extends BaseUnitTest {
 
     private ValidateUserLocationPresenter presenter;
 
+    private Location location;
+
     @Before
     public void setUp() {
         presenter = new ValidateUserLocationPresenter(locationView, callback);
+        location = new Location("test");
+        location.setLatitude(12.1212);
+        location.setLongitude(67.2232);
     }
 
     @Test
@@ -40,9 +52,6 @@ public class ValidateUserLocationPresenterTest extends BaseUnitTest {
     @Test
     public void testOnGetUserLocationShouldRequestSupervisorPassword() {
 
-        Location location = new Location("test");
-        location.setLatitude(12.1212);
-        location.setLongitude(67.2232);
         when(callback.getTargetCoordinates()).thenReturn(new LatLng(11.23232, 34.223222));
 
         presenter.onGetUserLocation(location);
@@ -54,14 +63,69 @@ public class ValidateUserLocationPresenterTest extends BaseUnitTest {
     @Test
     public void testOnGetUserLocationShouldInvokeLocationValidate() {
 
-        Location location = new Location("test");
-        location.setLatitude(12.1212);
-        location.setLongitude(67.2232);
         when(callback.getTargetCoordinates()).thenReturn(new LatLng(location.getLatitude(), location.getLongitude()));
 
         presenter.onGetUserLocation(location);
         verify(locationView).hideProgressDialog();
         verify(callback).getTargetCoordinates();
         verify(callback).onLocationValidated();
+    }
+
+    @Test
+    public void testOnGetUserLocationFailedShouldRequestForPasword() {
+        presenter.onGetUserLocationFailed();
+        verify(locationView).hideProgressDialog();
+        verify(callback).requestUserPassword();
+    }
+
+
+    @Test
+    public void testWaitForUserLocationShouldInvokeOnGetUserLocation() {
+        presenter = spy(presenter);
+        when(callback.getTargetCoordinates()).thenReturn(new LatLng(location.getLatitude(), location.getLongitude()));
+
+        when(locationView.getUserCurrentLocation()).thenReturn(location);
+        presenter.waitForUserLocation();
+
+        verify(presenter).onGetUserLocation(location);
+        verify(locationView).hideProgressDialog();
+        verify(callback).getTargetCoordinates();
+        verify(callback).onLocationValidated();
+    }
+
+
+    @Test
+    public void testWaitForUserLocationInvokeOnGetUserLocationFailed() {
+        presenter = spy(presenter);
+        when(locationView.getUserCurrentLocation()).thenAnswer(invocation -> {
+            Thread.sleep(3000);
+            return null;
+        });
+        ReflectionHelpers.callInstanceMethod(presenter, "waitForUserLocation", ClassParameter.from(long.class, 61000));
+
+        verify(presenter, timeout(ASYNC_TIMEOUT)).onGetUserLocationFailed();
+        verify(locationView, timeout(ASYNC_TIMEOUT)).hideProgressDialog();
+        verify(callback, timeout(ASYNC_TIMEOUT)).requestUserPassword();
+    }
+
+
+    @Test
+    public void testWaitForUserLocationTryEvery2SecondsUntilTimeout() {
+        AtomicInteger count = new AtomicInteger();
+        when(locationView.getUserCurrentLocation()).thenAnswer(invocation -> {
+            Thread.sleep(1000);
+            count.getAndIncrement();
+            return count.get() == 3 ? location : null;
+        });
+        when(callback.getTargetCoordinates()).thenReturn(new LatLng(location.getLatitude(), location.getLongitude()));
+
+
+        presenter.waitForUserLocation();
+        int ASYNC_TIMEOUT = 10 * 1000;
+        verify(locationView,timeout(ASYNC_TIMEOUT).times(3)).getUserCurrentLocation();
+        verify(locationView, timeout(ASYNC_TIMEOUT).times(2)).showProgressDialog(R.string.narrowing_location_title, R.string.narrowing_location_message);
+        verify(locationView, timeout(ASYNC_TIMEOUT)).hideProgressDialog();
+        verify(callback, timeout(ASYNC_TIMEOUT)).onLocationValidated();
+
     }
 }
