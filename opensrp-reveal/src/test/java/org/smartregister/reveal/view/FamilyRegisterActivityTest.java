dddc2726af605317dfb19a9277658aae81c1f0d2
Samuel Githengi <samuel.githengi@gmail.com>
2021-01-19 10:57:24 +0300

    Add unit tests

--- opensrp-reveal/src/test/java/org/smartregister/reveal/view/FamilyRegisterActivityTest.java
+++ opensrp-reveal/src/test/java/org/smartregister/reveal/view/FamilyRegisterActivityTest.java
@@ -2,17 +2,24 @@ package org.smartregister.reveal.view;
 
 import android.content.Intent;
 
+import androidx.fragment.app.Fragment;
+
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.robolectric.Robolectric;
-import org.robolectric.android.controller.ActivityController;
 import org.robolectric.util.ReflectionHelpers;
 import org.smartregister.Context;
 import org.smartregister.family.contract.FamilyRegisterContract;
 import org.smartregister.reveal.BaseUnitTest;
 import org.smartregister.service.ZiggyService;
+import org.smartregister.view.fragment.BaseRegisterFragment;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
@@ -30,16 +37,14 @@ public class FamilyRegisterActivityTest extends BaseUnitTest {
     @Mock
     private ZiggyService ziggyService;
 
-    private ActivityController<FamilyRegisterActivity> activityController;
-
     @Before
     public void setUp() {
         ReflectionHelpers.setField(Context.getInstance(), "ziggyService", ziggyService);
-        activityController = Robolectric.buildActivity(FamilyRegisterActivity.class);
+        activity = Robolectric.buildActivity(FamilyRegisterActivity.class).get();
     }
 
     @Test
-    public void testOnCreateShouldNotStartRegistration() throws Exception {
+    public void testOnCreateShouldNotStartRegistration() {
         activity = Robolectric.buildActivity(FamilyRegisterActivity.class).get();
         activity = spy(activity);
         activity.onCreate(null);
@@ -47,7 +52,7 @@ public class FamilyRegisterActivityTest extends BaseUnitTest {
     }
 
     @Test
-    public void testOnCreateShouldStartRegistration() throws Exception {
+    public void testOnCreateShouldStartRegistration() {
         Intent intent = new Intent();
         intent.putExtra("START_REGISTRATION", true);
         activity = Robolectric.buildActivity(FamilyRegisterActivity.class, intent).get();
@@ -55,5 +60,24 @@ public class FamilyRegisterActivityTest extends BaseUnitTest {
         activity.onCreate(null);
         verify(activity).startRegistration();
 
+    }
+
+    @Test
+    public void testInitializePresenterShouldInitializePresenter() {
+        assertNull(activity.presenter());
+        activity.initializePresenter();
+        assertNotNull(activity.presenter());
+        presenter = activity.presenter();
+
+    }
+
+    @Test
+    public void testGetRegisterFragmentShouldReturnRegisterFragment() {
+        MatcherAssert.assertThat(activity.getRegisterFragment(), Matchers.instanceOf(BaseRegisterFragment.class));
+    }
+
+    @Test
+    public void testGetOtherFragmentsReturnsEmptyArray() {
+        assertArrayEquals(new Fragment[0], activity.getOtherFragments());
     }
 }
