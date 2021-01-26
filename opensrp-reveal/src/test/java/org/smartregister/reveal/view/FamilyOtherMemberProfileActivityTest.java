f2a955b593d27e686d1a3375753e688a9ff1aba6
Samuel Githengi <samuel.githengi@gmail.com>
2021-01-26 12:35:22 +0300

    Unit test creation of option menu and click handers

--- opensrp-reveal/src/test/java/org/smartregister/reveal/view/FamilyOtherMemberProfileActivityTest.java
+++ opensrp-reveal/src/test/java/org/smartregister/reveal/view/FamilyOtherMemberProfileActivityTest.java
@@ -1,6 +1,8 @@
 package org.smartregister.reveal.view;
 
 import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.viewpager.widget.PagerAdapter;
 
@@ -10,11 +12,17 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.Mock;
 import org.robolectric.Robolectric;
+import org.robolectric.Shadows;
+import org.robolectric.fakes.RoboMenu;
+import org.robolectric.shadows.ShadowActivity;
 import org.robolectric.util.ReflectionHelpers;
 import org.smartregister.Context;
 import org.smartregister.family.util.Constants.INTENT_KEY;
 import org.smartregister.reveal.BaseUnitTest;
+import org.smartregister.reveal.R;
+import org.smartregister.reveal.contract.FamilyOtherMemberProfileContract;
 import org.smartregister.reveal.presenter.FamilyOtherMemberPresenter;
+import org.smartregister.view.activity.FormActivity;
 import org.smartregister.view.viewpager.OpenSRPViewPager;
 
 import java.util.ArrayList;
@@ -23,7 +31,10 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.robolectric.Shadows.shadowOf;
 import static org.smartregister.reveal.util.Constants.DatabaseKeys.STRUCTURE_ID;
 
 /**
@@ -36,13 +47,21 @@ public class FamilyOtherMemberProfileActivityTest extends BaseUnitTest {
     @Mock
     private OpenSRPViewPager mPager;
 
+    @Mock
+    private Menu menu;
+
     @Captor
     private ArgumentCaptor<PagerAdapter> pagerAdapterArgumentCaptor;
+
+    @Mock
+    private FamilyOtherMemberProfileContract.Presenter presenter;
+
+    private Intent intent;
 
     @Before
     public void setUp() {
         Context.bindtypes = new ArrayList<>();
-        Intent intent = new Intent();
+        intent = new Intent();
         intent.putExtra(INTENT_KEY.BASE_ENTITY_ID, "1d1232");
         intent.putExtra(INTENT_KEY.FAMILY_BASE_ENTITY_ID, "f_1d1232");
         intent.putExtra(INTENT_KEY.FAMILY_HEAD, "doe");
@@ -50,6 +69,7 @@ public class FamilyOtherMemberProfileActivityTest extends BaseUnitTest {
         intent.putExtra(INTENT_KEY.FAMILY_NAME, "Dolce");
         intent.putExtra(STRUCTURE_ID, "s_122323");
         activity = Robolectric.buildActivity(FamilyOtherMemberProfileActivity.class, intent).get();
+
     }
 
     @Test
@@ -71,5 +91,74 @@ public class FamilyOtherMemberProfileActivityTest extends BaseUnitTest {
         verify(mPager).setAdapter(pagerAdapterArgumentCaptor.capture());
         assertEquals(1, pagerAdapterArgumentCaptor.getValue().getCount());
         assertEquals("", pagerAdapterArgumentCaptor.getValue().getPageTitle(0));
+    }
+
+    @Test
+    public void testOnCreateOptionsMenuShouldHideAddMember() {
+        menu = new RoboMenu();
+        menu.add(R.id.add_member);
+        menu.add(R.id.action_archive);
+        activity.onCreateOptionsMenu(menu);
+        MenuItem item = menu.findItem(R.id.add_member);
+        assertFalse(item.isVisible());
+
+        MenuItem archive = menu.findItem(R.id.action_archive);
+        assertTrue(archive.isVisible());
+    }
+
+    @Test
+    public void testOnCreateOptionsMenuShouldHideArchiveForFamilyHead() {
+        menu = new RoboMenu();
+        menu.add(R.id.action_archive);
+        ReflectionHelpers.setField(activity, "isFamilyHead", true);
+        activity.onCreateOptionsMenu(menu);
+        MenuItem item = menu.findItem(R.id.action_archive);
+        assertFalse(item.isVisible());
+    }
+
+    @Test
+    public void testOnClickUpShouldReturnHome() {
+        activity = Robolectric.buildActivity(FamilyOtherMemberProfileActivity.class, intent).create().resume().get();
+        ShadowActivity shadowActivity = Shadows.shadowOf(activity);
+        shadowActivity.clickMenuItem(android.R.id.home);
+
+        assertTrue(activity.isFinishing());
+
+    }
+
+
+    @Test
+    public void testOnClickRegistrationShouldEditRegistration() {
+        ReflectionHelpers.setField(activity, "presenter", presenter);
+        menu = new RoboMenu();
+        activity.getMenuInflater().inflate(R.menu.menu_profile_activity, menu);
+        ShadowActivity shadowActivity = Shadows.shadowOf(activity);
+        shadowActivity.clickMenuItem(R.id.action_registration);
+        verify(presenter).onEditMemberDetails();
+
+    }
+
+    @Test
+    public void testOnClickRegistrationShouldInvokeArchive() {
+        ReflectionHelpers.setField(activity, "presenter", presenter);
+        menu = new RoboMenu();
+        activity.getMenuInflater().inflate(R.menu.menu_profile_activity, menu);
+        ShadowActivity shadowActivity = Shadows.shadowOf(activity);
+        shadowActivity.clickMenuItem(R.id.action_archive);
+        verify(presenter).onArchiveFamilyMember();
+
+    }
+
+    @Test
+    public void testOnClicKOthersShouldInvokeParent() {
+        menu = new RoboMenu();
+        activity.getMenuInflater().inflate(R.menu.menu_profile_activity, menu);
+        ShadowActivity shadowActivity = Shadows.shadowOf(activity);
+        shadowActivity.clickMenuItem(R.id.add_member);
+        verifyNoMoreInteractions(presenter);
+        Intent intentResult = shadowActivity.getNextStartedActivityForResult().intent;
+        assertNotNull(intentResult);
+        assertEquals(FormActivity.class, shadowOf(intentResult).getIntentClass());
+
     }
 }
