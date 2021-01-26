b3dbb3434a700f1250feb774dff59568ecaa6846
Samuel Githengi <samuel.githengi@gmail.com>
2021-01-26 11:04:46 +0300

    Unit test pager is setup

--- opensrp-reveal/src/test/java/org/smartregister/reveal/view/FamilyOtherMemberProfileActivityTest.java
+++ opensrp-reveal/src/test/java/org/smartregister/reveal/view/FamilyOtherMemberProfileActivityTest.java
@@ -2,17 +2,20 @@ package org.smartregister.reveal.view;
 
 import android.content.Intent;
 
+import androidx.viewpager.widget.PagerAdapter;
+
 import org.junit.Before;
 import org.junit.Test;
-import org.powermock.reflect.Whitebox;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
 import org.robolectric.Robolectric;
 import org.robolectric.util.ReflectionHelpers;
 import org.smartregister.Context;
-import org.smartregister.CoreLibrary;
-import org.smartregister.family.util.Constants;
 import org.smartregister.family.util.Constants.INTENT_KEY;
 import org.smartregister.reveal.BaseUnitTest;
 import org.smartregister.reveal.presenter.FamilyOtherMemberPresenter;
+import org.smartregister.view.viewpager.OpenSRPViewPager;
 
 import java.util.ArrayList;
 
@@ -20,6 +23,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.mockito.Mockito.verify;
 import static org.smartregister.reveal.util.Constants.DatabaseKeys.STRUCTURE_ID;
 
 /**
@@ -29,10 +33,15 @@ public class FamilyOtherMemberProfileActivityTest extends BaseUnitTest {
 
     private FamilyOtherMemberProfileActivity activity;
 
+    @Mock
+    private OpenSRPViewPager mPager;
+
+    @Captor
+    private ArgumentCaptor<PagerAdapter> pagerAdapterArgumentCaptor;
+
     @Before
     public void setUp() {
         Context.bindtypes = new ArrayList<>();
-        //ReflectionHelpers.setField(Context.getInstance(), "ziggyService", ziggyService);
         Intent intent = new Intent();
         intent.putExtra(INTENT_KEY.BASE_ENTITY_ID, "1d1232");
         intent.putExtra(INTENT_KEY.FAMILY_BASE_ENTITY_ID, "f_1d1232");
@@ -49,10 +58,18 @@ public class FamilyOtherMemberProfileActivityTest extends BaseUnitTest {
         activity.initializePresenter();
         assertNotNull(activity.presenter());
         assertFalse(ReflectionHelpers.getField(activity, "isFamilyHead"));
-        FamilyOtherMemberPresenter presenter= (FamilyOtherMemberPresenter) activity.presenter();
-        assertEquals("1d1232",presenter.getBaseEntityId());
-        assertEquals("f_1d1232", ReflectionHelpers.getField(presenter,"familyBaseEntityId"));
-        assertEquals("Dolce", ReflectionHelpers.getField(presenter,"familyName"));
+        FamilyOtherMemberPresenter presenter = (FamilyOtherMemberPresenter) activity.presenter();
+        assertEquals("1d1232", presenter.getBaseEntityId());
+        assertEquals("f_1d1232", ReflectionHelpers.getField(presenter, "familyBaseEntityId"));
+        assertEquals("Dolce", ReflectionHelpers.getField(presenter, "familyName"));
 
+    }
+
+    @Test
+    public void testSetupViewPagerShouldSetUpAdapter() {
+        activity.setupViewPager(mPager);
+        verify(mPager).setAdapter(pagerAdapterArgumentCaptor.capture());
+        assertEquals(1, pagerAdapterArgumentCaptor.getValue().getCount());
+        assertEquals("", pagerAdapterArgumentCaptor.getValue().getPageTitle(0));
     }
 }
