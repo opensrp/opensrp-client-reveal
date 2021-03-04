3ce63cf154e6e715e288fc8d9a9a76bccb579877
Linda Oscar Gcaba <lindagcaba@gmail.com>
2021-03-04 23:47:56 +0200

    -load ec_family which are not linked to compound_structure and populate them on dropdown

--- opensrp-reveal/src/main/java/org/smartregister/reveal/model/FamilyRegisterModel.java
+++ opensrp-reveal/src/main/java/org/smartregister/reveal/model/FamilyRegisterModel.java
@@ -1,16 +1,30 @@
 package org.smartregister.reveal.model;
 
+import net.sqlcipher.Cursor;
+import net.sqlcipher.database.SQLiteDatabase;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.smartregister.domain.Location;
 import org.smartregister.family.domain.FamilyEventClient;
 import org.smartregister.family.model.BaseFamilyRegisterModel;
 import org.smartregister.reveal.BuildConfig;
+import org.smartregister.reveal.application.RevealApplication;
+import org.smartregister.reveal.util.Constants;
 import org.smartregister.reveal.util.Constants.Properties;
+import org.smartregister.reveal.util.FamilyConstants;
 import org.smartregister.reveal.util.PreferencesUtil;
 import org.smartregister.util.JsonFormUtils;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import timber.log.Timber;
+
+import static com.vijay.jsonwizard.constants.JsonFormConstants.KEYS;
+import static com.vijay.jsonwizard.constants.JsonFormConstants.VALUES;
 import static org.smartregister.reveal.util.FamilyConstants.DatabaseKeys.FAMILY_NAME;
 import static org.smartregister.reveal.util.FamilyConstants.RELATIONSHIP.RESIDENCE;
 import static org.smartregister.util.JsonFormUtils.VALUE;
@@ -61,7 +75,38 @@ public class FamilyRegisterModel extends BaseFamilyRegisterModel {
         if (familyNameFieldJSONObject != null) {
             familyNameFieldJSONObject.put(VALUE, this.structureName);
         }
+        populateCompoundStructureOptions(form);
         return form;
+    }
+
+    private void populateCompoundStructureOptions(JSONObject form){
+        SQLiteDatabase database = RevealApplication.getInstance().getRepository().getReadableDatabase();
+        JSONArray keys = new JSONArray();
+        JSONArray values = new JSONArray();
+        Cursor cursor = null;
+        try{
+                String query = String.format("SELECT %s,%s,%s FROM %s WHERE %s IS NULL",Constants.DatabaseKeys.STRUCTURE_ID,Constants.DatabaseKeys.FIRST_NAME,Constants.DatabaseKeys.LAST_NAME, FamilyConstants.TABLE_NAME.FAMILY, FamilyConstants.DatabaseKeys.COMPOUND_STRUCTURE);
+                cursor = database.rawQuery(query,new String[]{});
+                while (cursor.moveToNext()) {
+                    String structureId = cursor.getString(cursor.getColumnIndex(Constants.DatabaseKeys.STRUCTURE_ID));
+                    String firsName = cursor.getString(cursor.getColumnIndex(Constants.DatabaseKeys.FIRST_NAME));
+                    String lastName = cursor.getString(cursor.getColumnIndex(Constants.DatabaseKeys.LAST_NAME));
+                    keys.put(structureId);
+                    values.put(String.format("%s %s",firsName,lastName));
+                }
+            } catch (Exception e) {
+                Timber.e(e, "Error find Families ");
+            } finally {
+                if (cursor != null)
+                    cursor.close();
+            }
+            JSONObject compoundStructureField = JsonFormUtils.getFieldJSONObject(JsonFormUtils.fields(form),FamilyConstants.FormKeys.COMPOUND_STRUCTURE);
+            try {
+                compoundStructureField.put(KEYS,keys);
+                compoundStructureField.put(VALUES, values);
+            } catch (JSONException e) {
+                Timber.e(e, "Error populating %s Options",FamilyConstants.FormKeys.COMPOUND_STRUCTURE);
+            }
     }
 
     public String getStructureId() {
