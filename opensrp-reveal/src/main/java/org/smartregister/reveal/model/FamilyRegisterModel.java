7a43e566a2474bb7275bd9094b60e1d98304af56
Linda Oscar Gcaba <lindagcaba@gmail.com>
2021-03-08 10:03:40 +0200

    -cleanup

--- opensrp-reveal/src/main/java/org/smartregister/reveal/model/FamilyRegisterModel.java
+++ opensrp-reveal/src/main/java/org/smartregister/reveal/model/FamilyRegisterModel.java
@@ -17,9 +17,7 @@ import org.smartregister.reveal.util.FamilyConstants;
 import org.smartregister.reveal.util.PreferencesUtil;
 import org.smartregister.util.JsonFormUtils;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import timber.log.Timber;
 
