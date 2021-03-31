69aef9598970664203dfd959c70f2ac68ac642d1
faiq <muhammad.faiq@venturedive.com>
2021-04-01 01:14:31 +0500

    add toolbar and back button to StructureTasksActivity

--- opensrp-reveal/src/main/java/org/smartregister/reveal/view/StructureTasksActivity.java
+++ opensrp-reveal/src/main/java/org/smartregister/reveal/view/StructureTasksActivity.java
@@ -1,7 +1,9 @@
 package org.smartregister.reveal.view;
 
 import android.os.Bundle;
+import android.view.View;
 
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.FragmentTransaction;
 
 import org.smartregister.reveal.R;
@@ -11,14 +13,34 @@ import org.smartregister.view.activity.MultiLanguageActivity;
 
 public class StructureTasksActivity extends MultiLanguageActivity {
 
+    private  StructureTasksFragment structureTasksFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_structure_tasks);
-        StructureTasksFragment structureTasksFragment = StructureTasksFragment.newInstance(this.getIntent().getExtras(), this);
+        setupToolbar();
+        initializeStructureFragment();
+    }
+
+    private void initializeStructureFragment() {
+        structureTasksFragment = StructureTasksFragment.newInstance(this.getIntent().getExtras(), this);
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         ft.add(R.id.fragment_container, structureTasksFragment).commit();
         getSupportFragmentManager().executePendingTransactions();
         structureTasksFragment.setStructure(getIntent().getStringExtra(Constants.Properties.LOCATION_UUID));
     }
+
+    private void setupToolbar() {
+        Toolbar toolbar = this.findViewById(R.id.summary_toolbar);
+        toolbar.setTitle(R.string.return_to_register);
+        this.setSupportActionBar(toolbar);
+        this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+
 }
