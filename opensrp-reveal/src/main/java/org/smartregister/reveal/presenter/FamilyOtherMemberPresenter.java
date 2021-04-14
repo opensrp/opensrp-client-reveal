ac3c8f4ef5175fc4f52772e79ea4258904c5329b
Linda Oscar Gcaba <lindagcaba@gmail.com>
2021-04-14 18:54:37 +0200

    Create dispense task for child that previously did not qualify but has now been updated to qualify.

--- opensrp-reveal/src/main/java/org/smartregister/reveal/presenter/FamilyOtherMemberPresenter.java
+++ opensrp-reveal/src/main/java/org/smartregister/reveal/presenter/FamilyOtherMemberPresenter.java
@@ -6,6 +6,9 @@ import android.content.DialogInterface;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.tuple.Triple;
+import org.joda.time.DateTime;
+import org.joda.time.Months;
+import org.joda.time.Years;
 import org.json.JSONObject;
 import org.smartregister.commonregistry.CommonPersonObject;
 import org.smartregister.commonregistry.CommonPersonObjectClient;
@@ -21,7 +24,12 @@ import org.smartregister.reveal.contract.FamilyProfileContract;
 import org.smartregister.reveal.interactor.RevealFamilyOtherMemberInteractor;
 import org.smartregister.reveal.model.FamilyProfileModel;
 import org.smartregister.reveal.util.AlertDialogUtils;
+import org.smartregister.reveal.util.Constants;
 import org.smartregister.reveal.util.FamilyJsonFormUtils;
+import org.smartregister.reveal.util.TaskUtils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import timber.log.Timber;
 
@@ -142,7 +150,7 @@ public class FamilyOtherMemberPresenter extends BaseFamilyOtherMemberProfileActi
             if (familyEventClient == null) {
                 return;
             }
-
+            updateMDADispenseTasksOnAgeChange(familyEventClient);
             profileInteractor.saveRegistration(familyEventClient, jsonString, true, this);
         } catch (Exception e) {
             getView().hideProgressDialog();
@@ -172,5 +180,25 @@ public class FamilyOtherMemberPresenter extends BaseFamilyOtherMemberProfileActi
 
     public void setStructureId(String structureId) {
         ((FamilyProfileModel) profileModel).setStructureId(structureId);
+    }
+
+    private void updateMDADispenseTasksOnAgeChange(FamilyEventClient familyEventClient) throws Exception {
+        JSONObject familyEventClientOriginal =  RevealApplication.getInstance().getContext().getEventClientRepository().getClientByBaseEntityId(familyEventClient.getClient().getBaseEntityId());
+        Date updateBirthDate = familyEventClient.getClient().getBirthdate();
+        int updateAge = Years.yearsBetween(new DateTime(updateBirthDate.getTime()), DateTime.now()).getYears();
+        int updateMonths =  Months.monthsBetween(new DateTime(updateBirthDate.getTime()),DateTime.now()).getMonths();
+        Date previouslyEnteredBirthDate =  new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").parse((String) familyEventClientOriginal.get("birthdate")) ;
+        int previouslyEnteredAge = Years.yearsBetween(new DateTime(previouslyEnteredBirthDate.getTime()), DateTime.now()).getYears();
+        int previouslyEnteredMonths =  Months.monthsBetween(new DateTime(previouslyEnteredBirthDate.getTime()),DateTime.now()).getMonths();
+
+        if(previouslyEnteredAge < Constants.MDA_MIN_AGE && previouslyEnteredMonths >= Constants.SMC_DISPENSE_MIN_MONTHS){
+            if(!(updateAge < Constants.MDA_MIN_AGE &&  updateMonths >= Constants.SMC_DISPENSE_MIN_MONTHS)){
+                //REMOVE/DEACTIVATE THE PREVIOUSLY CREATED TASK
+            }
+        } else {
+            if(updateAge < Constants.MDA_MIN_AGE &&  updateMonths >= Constants.SMC_DISPENSE_MIN_MONTHS){
+                TaskUtils.getInstance().generateMDADispenseTask(RevealApplication.getInstance().getContext().applicationContext(),familyEventClient.getClient().getBaseEntityId(),familyEventClient.getEvent().getLocationId());
+            }
+        }
     }
 }
