3a62f36d294068664a1934e3b243fdd6292128f4
faiq <muhammad.faiq@venturedive.com>
2021-04-14 13:54:57 +0500

    open family registration form from structure task details

--- opensrp-reveal/src/main/java/org/smartregister/reveal/contract/StructureTasksContract.java
+++ opensrp-reveal/src/main/java/org/smartregister/reveal/contract/StructureTasksContract.java
@@ -4,6 +4,7 @@ import android.content.Context;
 
 import org.smartregister.domain.Event;
 import org.smartregister.domain.Task;
+import org.smartregister.reveal.model.BaseTaskDetails;
 import org.smartregister.reveal.model.StructureTaskDetails;
 
 import java.util.List;
@@ -73,5 +74,7 @@ public interface StructureTasksContract {
         void updateTasks(String taskID, Task.TaskStatus taskStatus, String businessStatus, Set<Task> removedTasks);
 
         void displayResetTaskInfoDialog(StructureTaskDetails taskDetails);
+
+        void registerFamily(BaseTaskDetails taskDetails);
     }
 }
