28ef39a476ad3a9647c76317b5f46163c4d253c1
Linda Oscar Gcaba <lindagcaba@gmail.com>
2021-05-24 20:41:06 +0200

    cleanup

--- opensrp-reveal/src/main/java/org/smartregister/reveal/widget/RevealBarcodeFactory.java
+++ opensrp-reveal/src/main/java/org/smartregister/reveal/widget/RevealBarcodeFactory.java
@@ -2,7 +2,6 @@ package org.smartregister.reveal.widget;
 
 import android.app.Activity;
 import android.content.Context;
-import android.view.View;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
