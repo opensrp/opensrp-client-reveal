2a5d18b2ce2c430d0bf40c6d94cb354964fcee7c
Linda Oscar Gcaba <lindagcaba@gmail.com>
2021-06-09 16:34:26 +0200

    added operational areas to multiselect list

--- opensrp-reveal/src/main/java/org/smartregister/reveal/model/SprayAreaMultiSelectRepository.java
+++ opensrp-reveal/src/main/java/org/smartregister/reveal/model/SprayAreaMultiSelectRepository.java
@@ -5,10 +5,14 @@ import com.vijay.jsonwizard.interfaces.MultiSelectListRepository;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.smartregister.AllConstants;
 import org.smartregister.location.helper.LocationHelper;
+import org.smartregister.reveal.BuildConfig;
+import org.smartregister.reveal.util.Country;
 import org.smartregister.reveal.util.PreferencesUtil;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -18,8 +22,14 @@ public class SprayAreaMultiSelectRepository implements MultiSelectListRepository
     @Override
     public List<MultiSelectItem> fetchData() {
         List<MultiSelectItem> multiSelectItems = new ArrayList<>();
-        final String currentFacility = PreferencesUtil.getInstance().getCurrentFacility();
-        List<String> locationHierarchy = LocationHelper.getInstance().locationNamesFromHierarchy(currentFacility).stream().filter(name -> !name.equals(currentFacility)).collect(Collectors.toList());
+        List<String> locationHierarchy;
+        if(BuildConfig.BUILD_COUNTRY == Country.ZAMBIA){
+            //TODO: might just use this for both ZAMBIA and SENeGAL, check preferences first.
+            locationHierarchy  = Arrays.asList(PreferencesUtil.getInstance().getPreferenceValue(AllConstants.OPERATIONAL_AREAS).split(","));
+        } else {
+            final String currentFacility = PreferencesUtil.getInstance().getCurrentFacility();
+            locationHierarchy = LocationHelper.getInstance().locationNamesFromHierarchy(currentFacility).stream().filter(name -> !name.equals(currentFacility)).collect(Collectors.toList());
+        }
         JSONObject property = new JSONObject();
         try {
             property.put("presumed-id","err");
