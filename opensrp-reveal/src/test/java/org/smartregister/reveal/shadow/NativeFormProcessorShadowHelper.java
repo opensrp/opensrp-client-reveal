38c3a361b8baf42ef2c20ba8780e1bb5401582ac
rkodev <43806892+rkodev@users.noreply.github.com>
2021-07-09 08:45:36 +0300

    Update tests to  latest core

--- opensrp-reveal/src/test/java/org/smartregister/reveal/shadow/NativeFormProcessorShadowHelper.java
+++ opensrp-reveal/src/test/java/org/smartregister/reveal/shadow/NativeFormProcessorShadowHelper.java
@@ -6,24 +6,31 @@ import org.mockito.Mockito;
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
 import org.smartregister.domain.Location;
-import org.smartregister.reveal.util.NativeFormProcessor;
+import org.smartregister.reveal.util.NativeFormProcessorFactory;
+import org.smartregister.sync.ClientProcessorForJava;
+import org.smartregister.sync.helper.ECSyncHelper;
+import org.smartregister.util.NativeFormProcessor;
 
 import java.util.Date;
 
-@Implements(NativeFormProcessor.class)
+@Implements(NativeFormProcessorFactory.class)
 public class NativeFormProcessorShadowHelper {
 
     private static NativeFormProcessor processorSpy;
+    private static Location location = new Location();
+    private static Location structure = new Location();
 
     @Implementation
     public static NativeFormProcessor createInstance(String jsonString) throws JSONException {
-        processorSpy = Mockito.spy(new NativeFormProcessor(new JSONObject(jsonString)));
+        processorSpy = Mockito.spy(new NativeFormProcessor(new JSONObject(jsonString), 1, Mockito.mock(ClientProcessorForJava.class)));
+        Mockito.doReturn(getSyncHelper()).when(processorSpy).getSyncHelper();
         return processorSpy;
     }
 
     @Implementation
     public static NativeFormProcessor createInstance(JSONObject jsonObject) {
-        processorSpy = Mockito.spy(new NativeFormProcessor(jsonObject));
+        processorSpy = Mockito.spy(new NativeFormProcessor(jsonObject, 1, Mockito.mock(ClientProcessorForJava.class)));
+        Mockito.doReturn(getSyncHelper()).when(processorSpy).getSyncHelper();
         return processorSpy;
     }
 
@@ -32,28 +39,28 @@ public class NativeFormProcessorShadowHelper {
     }
 
     @Implementation
-    public Location getCurrentOperationalArea() {
-        Location location = new Location();
+    public static Location getCurrentOperationalArea() {
         location.setServerVersion(new Date().getTime());
         return location;
     }
 
     @Implementation
-    public Location getCurrentSelectedStructure() {
-        Location location = new Location();
-        location.setServerVersion(new Date().getTime());
-        return location;
+    public static Location getCurrentSelectedStructure() {
+        structure.setServerVersion(new Date().getTime());
+        return structure;
+    }
+
+    /**
+     * Mock sync helper to return a fake saved client
+     *
+     * @return
+     */
+    private static ECSyncHelper getSyncHelper() {
+        ECSyncHelper syncHelper = Mockito.mock(ECSyncHelper.class);
+        Mockito.doReturn(new JSONObject()).when(syncHelper).getClient(Mockito.anyString());
+        Mockito.doReturn(new JSONObject()).when(syncHelper).getClient(null);
+        return syncHelper;
     }
 
 
-    @Implementation
-    public NativeFormProcessor closeRegistrationID(String uniqueIDKey) {// Do Nothing
-        return null;
-    }
-
-    @Implementation
-    public NativeFormProcessor mergeAndSaveClient() {
-        // do nothing else
-        return processorSpy;
-    }
 }
